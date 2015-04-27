551a3d3e1b38634a97ae7f7413872d6447727350
Murat Fersatoglu <murat.fersatoglu@atmel.com>
2015-04-27 13:15:02 +0200

    Change flexcom_usart to flexcom

---
+++ drivers/serial/flexcom.c
@@ -0,0 +1,123 @@
+/* ----------------------------------------------------------------------------
+ *         SAM Software Package License
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2015, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ----------------------------------------------------------------------------
+ */
+
+/** \addtogroup usart_module Working with USART
+ * \section Purpose
+ * The USART driver provides the interface to configure and use the USART peripheral.\n
+ *
+ * The USART supports several kinds of communication modes such as full-duplex asynchronous/
+ * synchronous serial communication,RS485 with driver control signal,ISO7816,SPI and Test modes.
+ *
+ * To start a USART transfer with \ref dmad_module "DMA" support, the user could follow these steps:
+ * <ul>
+ * <li> Configure USART with expected mode and baudrate(see \ref USART_Configure), which could be done by:
+ * -# Resetting and disabling transmitter and receiver by setting US_CR(Control Register). </li>
+ * -# Configuring the USART in a specific mode by setting USART_MODE bits in US_MR(Mode Register) </li>
+ * -# Setting baudrate which is different from mode to mode.
+   </li>
+ * <li> Enable transmitter or receiver respectively by set US_CR_TXEN or US_CR_RXEN in US_CR.</li>
+ * <li> Read from or write to the peripheral with  \ref dmad_module </li>
+ * </ul>
+ *
+ * \section Usage
+ * <ul>
+ * <li>  Enable or disable USART transmitter or receiver using
+ * USART_SetTransmitterEnabled() and USART_SetReceiverEnabled().
+ * <li>  Enable or disable USART interrupt using USART_EnableIt() or USART_DisableIt().
+ * </li>
+ * </ul>
+ *
+ * For more accurate information, please look at the USART section of the
+ * Datasheet.
+ *
+ * Related files :\n
+ * \ref usart.c\n
+ * \ref usart.h\n
+*/
+
+/**
+ * \file
+ *
+ * Implementation of USART (Universal Synchronous Asynchronous Receiver Transmitter)
+ * controller.
+ *
+ */
+/*------------------------------------------------------------------------------
+ *         Headers
+ *------------------------------------------------------------------------------*/
+
+#include "chip.h"
+#include "serial/flexcom.h"
+
+#include "serial/usart.h"
+#include "bus/spi.h"
+#include "bus/twi.h"
+
+#include <assert.h>
+
+/*------------------------------------------------------------------------------
+ *         Exported functions
+ *------------------------------------------------------------------------------*/
+
+/**
+ * \brief Select a protocol for a FLEXCOM device
+ *
+ *
+ *  \param flexcom  Pointer to FLEXCOM peripheral to configure.
+ *  \param protocol  Protocol to use.
+ */
+void flexcom_select(Flexcom * flexcom, uint32_t protocol)
+{
+	assert(flexcom);
+	uint32_t current_protocol = flexcom->FLEX_MR;
+
+	USART_SetReceiverEnabled(&flexcom->usart, 0u);
+	
+	/* Shutdown previous protocol */
+	switch (current_protocol) {
+	case FLEX_MR_OPMODE_USART:
+		USART_SetReceiverEnabled(&flexcom->usart, 0u);
+		USART_SetTransmitterEnabled(&flexcom->usart, 0u);
+		break;
+	case FLEX_MR_OPMODE_SPI:
+		SPI_Disable(&flexcom->spi);
+		break;
+	case FLEX_MR_OPMODE_TWI:
+		TWI_Stop(&flexcom->twi);
+	default:
+		break;
+	}
+
+	assert(protocol & FLEX_MR_OPMODE_NO_COM ||
+	       protocol & FLEX_MR_OPMODE_USART ||
+	       FLEX_MR_OPMODE_SPI || FLEX_MR_OPMODE_TWI);
+	
+	/* Activate the new mode () */
+	flexcom->FLEX_MR = protocol;
+}
---
+++ drivers/serial/flexcom.h
@@ -0,0 +1,74 @@
+/* ----------------------------------------------------------------------------
+ *         SAM Software Package License 
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2015, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * \file
+ *
+ * \par Purpose
+ *
+ * This module provides several definitions and methods for using an USART
+ * peripheral.
+ *
+ * \par Usage
+ *
+ * -# Enable the USART peripheral clock in the PMC.
+ * -# Enable the required USART PIOs (see pio.h).
+ * -# Configure the UART by calling USART_Configure.
+ * -# Enable the transmitter and/or the receiver of the USART using
+ *    USART_SetTransmitterEnabled and USART_SetReceiverEnabled.
+ * -# Send data through the USART using the USART_Write methods.
+ * -# Receive data from the USART using the USART_Read functions; the availability of data can be polled
+ *    with USART_IsDataAvailable.
+ * -# Disable the transmitter and/or the receiver of the USART with
+ *    USART_SetTransmitterEnabled and USART_SetReceiverEnabled.
+ */
+
+#ifndef _FLEXCOM_
+#define _FLEXCOM_
+
+/*------------------------------------------------------------------------------
+ *         Headers
+ *------------------------------------------------------------------------------*/
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*------------------------------------------------------------------------------*/
+/*         Exported functions                                                   */
+/*------------------------------------------------------------------------------*/
+
+extern void flexcom_select(Flexcom * flexcom, uint32_t protocol);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* #ifndef _FLEXCOM_ */
