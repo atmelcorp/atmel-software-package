50a5dcc821a8ec6effda6d7cf04b96f0b96cf887
Murat Fersatoglu <murat.fersatoglu@atmel.com>
2015-05-18 15:18:09 +0200

    Fix missing bracket in sama5d4-ek board header

--- target/sama5d4/board_sama5d4-ek.h
+++ target/sama5d4/board_sama5d4-ek.h
@@ -38,7 +38,7 @@
 #define PIN_LED_2   {(PIO_PE28), PIOE, ID_PIOE, PIO_OUTPUT_0, PIO_DEFAULT}
 
 /** List of all LEDs definitions. */
-#define PINS_LEDS   PIN_LED_0, PIN_LED_1,PIN_LED_2
+#define PINS_LEDS   {PIN_LED_0, PIN_LED_1,PIN_LED_2}
 
 /** Push button #1 definition. Attributes = pull-up + debounce + interrupt on rising edge. */
 #define PIN_PUSHBUTTON_1    {PIO_PE13, PIOE, ID_PIOE, PIO_INPUT, PIO_PULLUP | PIO_DEBOUNCE | PIO_IT_FALL_EDGE}
