9ece194e752c8df0ad2c08d5d0205e3b62fa21bd
Didier Fronek <didier.fronek@atmel.com>
2015-06-18 16:06:19 +0200

    Cleanup QT1070 driver

--- drivers/video/qt1070.c
+++ drivers/video/qt1070.c
@@ -1,7 +1,7 @@
 /* ----------------------------------------------------------------------------
  *         SAM Software Package License
  * ----------------------------------------------------------------------------
- * Copyright (c) 2011, Atmel Corporation
+ * Copyright (c) 2015, Atmel Corporation
  *
  * All rights reserved.
  *
@@ -42,155 +42,248 @@
 #include "video/qt1070.h"
 
 /*----------------------------------------------------------------------------
+ *         Local Definitions
+ *----------------------------------------------------------------------------*/
+
+/** Slave address */
+#define QT1070_SLAVE_ADDRESS    0x1B
+
+/** Internal Register Address Allocation */
+
+/** Chip ID register*/
+#define QT1070_CHIP_ID              0
+
+/** Firmware version register*/
+#define QT1070_REG_FIRMWARE_VERSION 1
+
+/** Detection status*/
+#define QT1070_REG_DETECTION_STATUS 2
+
+/** Key status*/
+#define QT1070_REG_KEY_STATUS       3
+
+/** Key signal */
+#define QT1070_REG_KEY0_SIGNAL_MSB  4
+#define QT1070_REG_KEY0_SIGNAL_LSB  5
+#define QT1070_REG_KEY1_SIGNAL_MSB  6
+#define QT1070_REG_KEY1_SIGNAL_LSB  7
+#define QT1070_REG_KEY2_SIGNAL_MSB  8
+#define QT1070_REG_KEY2_SIGNAL_LSB  9
+#define QT1070_REG_KEY3_SIGNAL_MSB  10
+#define QT1070_REG_KEY3_SIGNAL_LSB  11
+#define QT1070_REG_KEY4_SIGNAL_MSB  12
+#define QT1070_REG_KEY4_SIGNAL_LSB  13
+#define QT1070_REG_KEY5_SIGNAL_MSB  14
+#define QT1070_REG_KEY5_SIGNAL_LSB  15
+#define QT1070_REG_KEY6_SIGNAL_MSB  16
+#define QT1070_REG_KEY6_SIGNAL_LSB  17
+
+/** Reference date */
+#define QT1070_REG_REFDATA0_MSB     18
+#define QT1070_REG_REFDATA0_LSB     19
+#define QT1070_REG_REFDATA1_MSB     20
+#define QT1070_REG_REFDATA1_LSB     21
+#define QT1070_REG_REFDATA2_MSB     22
+#define QT1070_REG_REG_REFDATA2_LSB 23
+#define QT1070_REG_REFDATA3_MSB     24
+#define QT1070_REG_REG_REFDATA3_LSB 25
+#define QT1070_REG_REFDATA4_MSB     26
+#define QT1070_REG_REFDATA4_LSB     27
+#define QT1070_REG_REFDATA5_MSB     28
+#define QT1070_REG_REFDATA5_LSB     29
+#define QT1070_REG_REFDATA6_MSB     30
+#define QT1070_REG_REFDATA6_LSB     31
+
+/** Negative threshold level */
+#define QT1070_REG_NTHR_KEY0        32
+#define QT1070_REG_NTHR_KEY1        33
+#define QT1070_REG_NTHR_KEY2        34
+#define QT1070_REG_NTHR_KEY3        35
+#define QT1070_REG_NTHR_KEY4        36
+#define QT1070_REG_NTHR_KEY5        37
+#define QT1070_REG_NTHR_KEY6        38
+
+/** Adjacent key suppression level */
+#define QT1070_REG_AVEAKS_KEY0      39
+#define QT1070_REG_AVEAKS_KEY1      40
+#define QT1070_REG_AVEAKS_KEY2      41
+#define QT1070_REG_AVEAKS_KEY3      42
+#define QT1070_REG_AVEAKS_KEY4      43
+#define QT1070_REG_AVEAKS_KEY5      44
+#define QT1070_REG_AVEAKS_KEY6      45
+
+/** Detection interator conter for key*/
+#define QT1070_REG_DI_KEY0          46
+#define QT1070_REG_DI_KEY1          47
+#define QT1070_REG_DI_KEY2          48
+#define QT1070_REG_DI_KEY3          49
+#define QT1070_REG_DI_KEY4          50
+#define QT1070_REG_DI_KEY5          51
+#define QT1070_REG_DI_KEY6          52
+
+/** Low power mode */
+#define QT1070_REG_LOWPOWER_MODE    54
+
+/** Maximum on duration */
+#define QT1070_REG_MAX_DURATION     55
+
+/** Calibrate */
+#define QT1070_REG_CALIRATE         56
+
+/** Reset */
+#define QT1070_REG_RESET            57
+
+/** Detection Status. */
+
+/** This bit is set during a calibration sequence.*/
+#define QT1070_CALIBRATE_BIT        7
+
+/** This bit is set if the time to acquire all key signals exceeds 8 ms*/
+#define QT1070_OVERFLOW_BIT         6
+
+/** This bit is set if Comms mode is enabled. */
+#define QT1070_COMMSENABLED_BIT     5
+
+/** This bit is set if any keys are in detect. */
+#define QT1070_TOUCH_BIT            0
+
+/*----------------------------------------------------------------------------
  *        Local functions
  *----------------------------------------------------------------------------*/
 
 /**
  * \brief Read one byte of data from QT1070 Register.
  *
- * \param pTwid   Pointer to twi driver structure.
- * \param regAddr Register address to read.
+ * \param twid   Pointer to twi driver structure.
+ * \param reg_addr Register address to read.
  * \return value in the given register.
  */
-static uint8_t
-QT1070_ReadReg(struct _twid * pTwid, uint8_t regAddr)
+static uint8_t _qt1070_read_reg(struct _twid *twid, uint8_t reg_addr)
 {
 	uint8_t data;
-	twid_write(pTwid, QT1070_SLAVE_ADDRESS, 0, 0, &regAddr, 1, 0);
-	twid_read(pTwid, QT1070_SLAVE_ADDRESS, 0, 0, &data, 1, 0);
+	twid_write(twid, QT1070_SLAVE_ADDRESS, 0, 0, &reg_addr, 1, 0);
+	twid_read(twid, QT1070_SLAVE_ADDRESS, 0, 0, &data, 1, 0);
 	return data;
 }
 
 /**
  * \brief  Write one byte of data to QT1070 Register.
  *
- * \param pTwid   Pointer to twi driver structure.
- * \param regAddr Register address to write.
+ * \param twid   Pointer to twi driver structure.
+ * \param reg_addr Register address to write.
  * \param data    Data to write.
  */
-static void
-QT1070_WriteReg(struct _twid * pTwid, uint32_t regAddr, uint8_t data)
+static void _qt1070_write_reg(struct _twid *twid, uint32_t reg_addr, uint8_t data)
 {
-	twid_write(pTwid, QT1070_SLAVE_ADDRESS, regAddr, 1, &data, 1, 0);
+	twid_write(twid, QT1070_SLAVE_ADDRESS, reg_addr, 1, &data, 1, 0);
 }
 
 /*----------------------------------------------------------------------------
  *        Exported functions
  *----------------------------------------------------------------------------*/
+
 /**
- * \brief  Get qt1070 chip ID.
+ * \brief  Get chip ID.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \return Chip Id
  */
-
-uint8_t
-QT1070_GetChipId(struct _twid * pTwid)
+uint8_t qt1070_get_chip_id(struct _twid *twid)
 {
-	return QT1070_ReadReg(pTwid, QT1070_CHIP_ID);
+	return _qt1070_read_reg(twid, QT1070_CHIP_ID);
 }
 
 /**
- * \brief  Get qt1070 firmware version number.
+ * \brief  Get firmware version number.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \return Firmware version number.
  */
-
-uint8_t
-QT1070_GetFirmwareVersion(struct _twid * pTwid)
+uint8_t qt1070_get_firmware_version(struct _twid *twid)
 {
-	return QT1070_ReadReg(pTwid, QT1070_REG_FIRMWARE_VERSION);
+	return _qt1070_read_reg(twid, QT1070_REG_FIRMWARE_VERSION);
 }
 
 /**
-* \brief  Get qt1070 detection status.
+* \brief  Get detection status.
 *
-* \param pTwid   Pointer to twi driver structure.
+* \param twid   Pointer to twi driver structure.
 * \return Dectection status.
 */
-
-uint8_t
-QT1070_GetDetection_Status(struct _twid * pTwid)
+uint8_t qt1070_get_detection_status(struct _twid *twid)
 {
-	return QT1070_ReadReg(pTwid, QT1070_REG_DETECTION_STATUS);
+	return _qt1070_read_reg(twid, QT1070_REG_DETECTION_STATUS);
 }
 
 /**
- * \brief  Get qt1070 Key status.
+ * \brief  Get Key status.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \return Key status.
  */
-uint8_t
-QT1070_GetKey_Status(struct _twid * pTwid)
+uint8_t qt1070_get_key_status(struct _twid *twid)
 {
-	return QT1070_ReadReg(pTwid, QT1070_REG_KEY_STATUS);
+	return _qt1070_read_reg(twid, QT1070_REG_KEY_STATUS);
 }
 
 /**
- * \brief  Get qt1070 key signal value in the given Key. These are the key's
+ * \brief  Get key signal value in the given Key. These are the key's
  * of 16-bit key signals which are accessed as two 8-bit bytes,stored MSB first
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \param key     Key index.
  * \return Key signal value.
  */
-uint16_t
-QT1070_GetKey_Signal(struct _twid * pTwid, uint8_t key)
+uint16_t qt1070_get_key_signal(struct _twid *twid, uint8_t key)
 {
 	uint8_t data[2];
-	data[0] = QT1070_ReadReg(pTwid, QT1070_REG_KEY0_SIGNAL_MSB + key * 2);
-	data[1] = QT1070_ReadReg(pTwid, QT1070_REG_KEY0_SIGNAL_LSB + key * 2);
+	data[0] = _qt1070_read_reg(twid, QT1070_REG_KEY0_SIGNAL_MSB + key * 2);
+	data[1] = _qt1070_read_reg(twid, QT1070_REG_KEY0_SIGNAL_LSB + key * 2);
 	return (data[0] << 8) | data[1];
 }
 
 /**
- * \brief  Get qt1070 key reference data in the given Key. These are the key's
+ * \brief  Get key reference data in the given Key. These are the key's
  * of 16-bit key reference data which are accessed as two 8-bit bytes, stored MSB first
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \param key     Key index.
  * \return Key reference data.
  */
-uint16_t
-QT1070_GetKey_Reference(struct _twid * pTwid, uint8_t key)
+uint16_t qt1070_get_key_reference(struct _twid* twid, uint8_t key)
 {
 	uint8_t data[2];
-	data[0] = QT1070_ReadReg(pTwid, QT1070_REG_REFDATA0_MSB + key * 2);
-	data[1] = QT1070_ReadReg(pTwid, QT1070_REG_REFDATA0_LSB + key * 2);
+	data[0] = _qt1070_read_reg(twid, QT1070_REG_REFDATA0_MSB + key * 2);
+	data[1] = _qt1070_read_reg(twid, QT1070_REG_REFDATA0_LSB + key * 2);
 	return (data[0] << 8) | data[1];
 }
 
 /**
  * \brief  Set the threshold value for the given Key.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \param key     Key index.
  * \param threshold Threshold value.
  */
-void
-QT1070_SetThreshold(struct _twid * pTwid, uint8_t key, uint8_t threshold)
+void qt1070_set_threshold(struct _twid *twid, uint8_t key, uint8_t threshold)
 {
 	// Do not use a setting of 0 as this causes a key to go into detection
 	// when its signal is equal to its reference.
-	if (threshold) {
-		QT1070_WriteReg(pTwid, QT1070_REG_NTHR_KEY0 + key, threshold);
-	}
+	if (threshold)
+		_qt1070_write_reg(twid, QT1070_REG_NTHR_KEY0 + key, threshold);
 }
 
 /**
  * \brief  Set Averaging factor and adjacent key suppression for the given Key.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \param key     Key index.
- * \param Ave     Averaging factor.
- * \param Aks     AKS group index.
+ * \param ave     Averaging factor.
+ * \param aks     AKS group index.
  */
-void
-QT1070_SetAveAks(struct _twid * pTwid, uint8_t key, uint8_t Ave, uint8_t Aks)
+void qt1070_set_ave_aks(struct _twid *twid, uint8_t key, uint8_t ave, uint8_t aks)
 {
-	QT1070_WriteReg(pTwid, QT1070_REG_AVEAKS_KEY0 + key, (Ave << 3) | Aks);
+	_qt1070_write_reg(twid, QT1070_REG_AVEAKS_KEY0 + key, (ave << 3) | aks);
 }
 
 /**
@@ -198,15 +291,13 @@ QT1070_SetAveAks(struct _twid * pTwid, uint8_t key, uint8_t Ave, uint8_t Aks)
  * of consecutive measurement that must be confirmed as having passed the key threshold
  * before that key is registered as being in detect.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  * \param key     Key index.
  * \param di      DI level.
  */
-
-void
-QT1070_SetDetectionIntegrator(struct _twid * pTwid, uint8_t key, uint8_t di)
+void qt1070_set_detection_integrator(struct _twid *twid, uint8_t key, uint8_t di)
 {
-	QT1070_WriteReg(pTwid, QT1070_REG_DI_KEY0 + key, di);
+	_qt1070_write_reg(twid, QT1070_REG_DI_KEY0 + key, di);
 }
 
 /**
@@ -214,23 +305,19 @@ QT1070_SetDetectionIntegrator(struct _twid * pTwid, uint8_t key, uint8_t di)
  * register is set when the calibration begins and clears when the calibration
  * has finished.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  */
-
-void
-QT1070_StartCalibrate(struct _twid * pTwid)
+void qt1070_start_calibrate(struct _twid *twid)
 {
-	QT1070_WriteReg(pTwid, QT1070_REG_CALIRATE, 1);
+	_qt1070_write_reg(twid, QT1070_REG_CALIRATE, 1);
 }
 
 /**
- * \brief Reset the qt1070 device.
+ * \brief Reset the device.
  *
- * \param pTwid   Pointer to twi driver structure.
+ * \param twid   Pointer to twi driver structure.
  */
-
-void
-QT1070_StartReset(struct _twid * pTwid)
+void qt1070_start_reset(struct _twid *twid)
 {
-	QT1070_WriteReg(pTwid, QT1070_REG_RESET, 1);
+	_qt1070_write_reg(twid, QT1070_REG_RESET, 1);
 }
--- drivers/video/qt1070.h
+++ drivers/video/qt1070.h
@@ -1,7 +1,7 @@
 /* ----------------------------------------------------------------------------
  *         SAM Software Package License
  * ----------------------------------------------------------------------------
- * Copyright (c) 2011, Atmel Corporation
+ * Copyright (c) 2015, Atmel Corporation
  *
  * All rights reserved.
  *
@@ -34,120 +34,115 @@
   *
   */
 
-#ifndef QT1070_H
-#define QT1070_H
-
-#include "peripherals/twid.h"
+#ifndef _QT1070_H_
+#define _QT1070_H_
 
 /*----------------------------------------------------------------------------
- *         Definitions
+ *        Headers
  *----------------------------------------------------------------------------*/
-/** Slave address */
-#define QT1070_SLAVE_ADDRESS    0x1B
 
-/** Internal Register Address Allocation */
-
-/** Chip ID register*/
-#define QT1070_CHIP_ID          0
-/** Firmware version register*/
-#define QT1070_REG_FIRMWARE_VERSION 1
-/** Detection status*/
-#define QT1070_REG_DETECTION_STATUS 2
-/** Key status*/
-#define QT1070_REG_KEY_STATUS       3
-/** Key signal */
-#define QT1070_REG_KEY0_SIGNAL_MSB  4
-#define QT1070_REG_KEY0_SIGNAL_LSB  5
-#define QT1070_REG_KEY1_SIGNAL_MSB  6
-#define QT1070_REG_KEY1_SIGNAL_LSB  7
-#define QT1070_REG_KEY2_SIGNAL_MSB  8
-#define QT1070_REG_KEY2_SIGNAL_LSB  9
-#define QT1070_REG_KEY3_SIGNAL_MSB  10
-#define QT1070_REG_KEY3_SIGNAL_LSB  11
-#define QT1070_REG_KEY4_SIGNAL_MSB  12
-#define QT1070_REG_KEY4_SIGNAL_LSB  13
-#define QT1070_REG_KEY5_SIGNAL_MSB  14
-#define QT1070_REG_KEY5_SIGNAL_LSB  15
-#define QT1070_REG_KEY6_SIGNAL_MSB  16
-#define QT1070_REG_KEY6_SIGNAL_LSB  17
-
-/** Reference date */
-#define QT1070_REG_REFDATA0_MSB     18
-#define QT1070_REG_REFDATA0_LSB     19
-#define QT1070_REG_REFDATA1_MSB     20
-#define QT1070_REG_REFDATA1_LSB     21
-#define QT1070_REG_REFDATA2_MSB     22
-#define QT1070_REG_REG_REFDATA2_LSB 23
-#define QT1070_REG_REFDATA3_MSB     24
-#define QT1070_REG_REG_REFDATA3_LSB 25
-#define QT1070_REG_REFDATA4_MSB     26
-#define QT1070_REG_REFDATA4_LSB     27
-#define QT1070_REG_REFDATA5_MSB     28
-#define QT1070_REG_REFDATA5_LSB     29
-#define QT1070_REG_REFDATA6_MSB     30
-#define QT1070_REG_REFDATA6_LSB     31
-
-/** Negative threshold level */
-#define QT1070_REG_NTHR_KEY0        32
-#define QT1070_REG_NTHR_KEY1        33
-#define QT1070_REG_NTHR_KEY2        34
-#define QT1070_REG_NTHR_KEY3        35
-#define QT1070_REG_NTHR_KEY4        36
-#define QT1070_REG_NTHR_KEY5        37
-#define QT1070_REG_NTHR_KEY6        38
-
-/** Adjacent key suppression level */
-#define QT1070_REG_AVEAKS_KEY0      39
-#define QT1070_REG_AVEAKS_KEY1      40
-#define QT1070_REG_AVEAKS_KEY2      41
-#define QT1070_REG_AVEAKS_KEY3      42
-#define QT1070_REG_AVEAKS_KEY4      43
-#define QT1070_REG_AVEAKS_KEY5      44
-#define QT1070_REG_AVEAKS_KEY6      45
-
-/** Detection interator conter for key*/
-#define QT1070_REG_DI_KEY0          46
-#define QT1070_REG_DI_KEY1          47
-#define QT1070_REG_DI_KEY2          48
-#define QT1070_REG_DI_KEY3          49
-#define QT1070_REG_DI_KEY4          50
-#define QT1070_REG_DI_KEY5          51
-#define QT1070_REG_DI_KEY6          52
-
-/** Low power mode */
-#define QT1070_REG_LOWPOWER_MODE    54
-/** Maximum on duration */
-#define QT1070_REG_MAX_DURATION     55
-/** Calibrate */
-#define QT1070_REG_CALIRATE         56
-/** Reset */
-#define QT1070_REG_RESET            57
-
-/** Detection Status. */
-/** This bit is set during a calibration sequence.*/
-#define QT_CALIBRATE_BIT        7
-/** This bit is set if the time to acquire all key signals exceeds 8 ms*/
-#define QT_OVERFLOW_BIT         6
-/** This bit is set if Comms mode is enabled. */
-#define QT_COMMSENABLED_BIT     5
-/** This bit is set if any keys are in detect. */
-#define QT_TOUCH_BIT            0
+#include "peripherals/twid.h"
 
 /*----------------------------------------------------------------------------
  *         Exported functions
  *----------------------------------------------------------------------------*/
 
-extern uint8_t QT1070_GetChipId(struct _twid * pTwid);
-extern uint8_t QT1070_GetFirmwareVersion(struct _twid * pTwid);
-extern uint8_t QT1070_GetDetection_Status(struct _twid * pTwid);
-extern uint8_t QT1070_GetKey_Status(struct _twid * pTwid);
-extern uint16_t QT1070_GetKey_Signal(struct _twid * pTwid, uint8_t key);
-extern uint16_t QT1070_GetKey_Reference(struct _twid * pTwid, uint8_t key);
-extern void QT1070_SetThreshold(struct _twid * pTwid, uint8_t key, uint8_t threshold);
-extern void QT1070_SetAveAks(struct _twid * pTwid, uint8_t key, uint8_t Ave,
-			     uint8_t Aks);
-extern void QT1070_SetDetectionIntegrator(struct _twid * pTwid, uint8_t key,
-					  uint8_t di);
-extern void QT1070_StartCalibrate(struct _twid * pTwid);
-extern void QT1070_StartReset(struct _twid * pTwid);
-#endif				// QT1070_H
+/**
+ * \brief  Get chip ID.
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \return Chip Id
+ */
+extern uint8_t qt1070_get_chip_id(struct _twid *twid);
+
+/**
+ * \brief  Get firmware version number.
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \return Firmware version number.
+ */
+extern uint8_t qt1070_get_firmware_version(struct _twid *twid);
+
+/**
+* \brief  Get detection status.
+*
+* \param twid   Pointer to twi driver structure.
+* \return Dectection status.
+*/
+extern uint8_t qt1070_get_detection_status(struct _twid *twid);
+
+/**
+ * \brief  Get Key status.
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \return Key status.
+ */
+extern uint8_t qt1070_get_key_status(struct _twid *twid);
+
+/**
+ * \brief  Get key signal value in the given Key. These are the key's
+ * of 16-bit key signals which are accessed as two 8-bit bytes,stored MSB first
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \param key     Key index.
+ * \return Key signal value.
+ */
+extern uint16_t qt1070_get_key_signal(struct _twid *twid, uint8_t key);
+
+/**
+ * \brief  Get key reference data in the given Key. These are the key's
+ * of 16-bit key reference data which are accessed as two 8-bit bytes, stored MSB first
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \param key     Key index.
+ * \return Key reference data.
+ */
+extern uint16_t qt1070_get_key_reference(struct _twid* twid, uint8_t key);
+
+/**
+ * \brief  Set the threshold value for the given Key.
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \param key     Key index.
+ * \param threshold Threshold value.
+ */
+extern void qt1070_set_threshold(struct _twid *twid, uint8_t key, uint8_t threshold);
+
+/**
+ * \brief  Set Averaging factor and adjacent key suppression for the given Key.
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \param key     Key index.
+ * \param ave     Averaging factor.
+ * \param aks     AKS group index.
+ */
+extern void qt1070_set_ave_aks(struct _twid *twid, uint8_t key, uint8_t ave, uint8_t aks);
+
+/**
+ * \brief Set DI level for the given Key. This 8-bit value controls the number
+ * of consecutive measurement that must be confirmed as having passed the key threshold
+ * before that key is registered as being in detect.
+ *
+ * \param twid   Pointer to twi driver structure.
+ * \param key     Key index.
+ * \param di      DI level.
+ */
+extern void qt1070_set_detection_integrator(struct _twid *twid, uint8_t key, uint8_t di);
+
+/**
+ * \brief Start a calibration cycle, the CALIBTATE flag in the detection status
+ * register is set when the calibration begins and clears when the calibration
+ * has finished.
+ *
+ * \param twid   Pointer to twi driver structure.
+ */
+extern void qt1070_start_calibrate(struct _twid *twid);
+
+/**
+ * \brief Reset the device.
+ *
+ * \param twid   Pointer to twi driver structure.
+ */
+extern void qt1070_start_reset(struct _twid *twid);
+
+#endif /* _QT1070_H_ */
