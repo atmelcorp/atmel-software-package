b1b446c09e2a59ae8cbe7fe754006452b24a284b
Aubin Constans <aubin.constans@atmel.com>
2015-08-05 15:43:41 +0200

    peripherals/tdes: slight comment fixes
    Signed-off-by: Aubin Constans <aubin.constans@atmel.com>

--- drivers/peripherals/tdes.c
+++ drivers/peripherals/tdes.c
@@ -29,34 +29,46 @@
 
 /** \addtogroup tdes_module Working with TDES
  * \ingroup peripherals_module
- * The ADC driver provides the interface to configure and use the TDES peripheral.
+ * The TDES driver provides the interface to configure and use the TDES
+ * peripheral.
  * \n
  *
- * The Data Encryption Standard (DES) and the Triple Data Encryption Algorithm (TDES) specify
- * FIPS-approved cryptographic algorithms that can be used to protect electronic data. The TDES
- * bit in the TDES Mode Register (TDES_MR) is used to select either the single DES or the Triple
- * DES mode by function tdes_configure().
- * A DES is capable of using cryptographic keys of 64 bits to encrypt and decrypt data in blocks of
- * 64 bits, Date input can be set with tdes_set_input(). This 64-bit key is defined in the Key 1 Word
- * Registers (TDES_KEY1WRx) and set by tdes_write_key1. A TDES key consists of three DES keys,
- * which is also referred to as a key bundle. These three 64-bit keys are defined, respectively,
- * in the Key 1, 2 and 3 Word Registers (TDES_KEY1WRx, TDES_KEY2WRx and TDES_KEY3WRx).
- * In Triple DES mode (TDESMOD set to 1), the KEYMOD bit in the TDES Mode Register is used to choose between a two- and a three-key
- * algorithm:
+ * The Data Encryption Standard (DES) and the Triple Data Encryption Algorithm
+ * (TDES) specify FIPS-approved cryptographic algorithms that can be used to
+ * protect electronic data.
+ * The TDES bit in the TDES Mode Register (TDES_MR) is used to select either the
+ * single DES or the Triple DES mode (make use of the tdes_configure()
+ * function).
  *
- * To Enable a TDES encryption and decrypt,the user has to follow these few steps:
+ * The DES uses 64-bit cryptographic keys to encrypt and decrypt 64-bit data
+ * blocks. Input data can be fed calling tdes_set_input().
+ * The 64-bit key is defined in the Key 1 Word Registers (TDES_KEY1WRx) and set
+ * by tdes_write_key1().
+ *
+ * The Triple DES key consists in three DES keys, which set is also referred to
+ * as a key bundle. These three 64-bit keys are defined, respectively, in the
+ * Key 1, 2 and 3 Word Registers (TDES_KEY1WRx, TDES_KEY2WRx and TDES_KEY3WRx).
+ * In Triple DES mode (TDES_MR:TDESMOD set to 1), the TDES_MR:KEYMOD bit is used
+ * to select either the two- or three-key algorithm.
+ *
+ * In order to perform TDES encryption/decryption, the user has to follow these
+ * few steps:
  * <ul>
- * <li> A software triggered hardware reset of the TDES interface is performed by tdes_soft_reset().</li>
- * <li> Configure TDES algorithm mode, key mode, start mode and operation mode by tdes_configure(). </li>
- * <li> Set DES key with fucntion tdes_write_key1(), tdes_write_key2(), or tdes_write_key3(). </li>
- * <li> To start the encryption or the decryption process with tdes_start()</li>
- * <li> To get the encryption or decryption reslut by tdes_get_output() </li>
+ * <li> A software-triggered hardware reset of the TDES interface is performed
+ * by tdes_soft_reset().</li>
+ * <li> Configure TDES algorithm mode, key mode, start mode and operation mode
+ * by means of tdes_configure().</li>
+ * <li> Set DES keys with function tdes_write_key1(), tdes_write_key2(), and
+ * tdes_write_key3().</li>
+ * <li> To start encrypting or decrypting call tdes_start().</li>
+ * <li> Retrieve the encryption or decryption result by means of
+ * tdes_get_output().</li>
  * </ul>
  *
  * For more accurate information, please look at the TDES section of the
  * Datasheet.
  *
- * Related files :\n
+ * Related files:\n
  * \ref tdes.c\n
  * \ref tdes.h\n
  */
@@ -65,7 +77,7 @@
 /**
  * \file
  *
- * Implementation of Triple Data Encryption Standard (TDES).
+ * Implementation of the Triple Data Encryption Algorithm (TDES).
  *
  */
 
