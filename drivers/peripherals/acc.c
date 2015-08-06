f26d4259a1d4eddddbc954560412a7722ceca546
Aubin Constans <aubin.constans@atmel.com>
2015-08-06 15:02:37 +0200

    peripherals/acc: move interface doc from source to header
    Signed-off-by: Aubin Constans <aubin.constans@atmel.com>

--- drivers/peripherals/acc.c
+++ drivers/peripherals/acc.c
@@ -53,17 +53,8 @@
  *         Exported functions
  *----------------------------------------------------------------------------*/
 
-/**
- * \brief Initialize the ACC controller.
- *
- * \param p_acc Pointer to an Acc instance.
- * \param select_plus input connected to inp, 0~7.
- * \param select_minus input connected to inm,0~7.
- * \param edge_type CF flag triggering mode, use pattern defined in the device header file.
- * \param invert Invert comparator output.
- */
 void acc_init(Acc *p_acc, uint32_t select_plus, uint32_t select_minus,
-		uint32_t edge_type, uint32_t invert)
+              uint32_t edge_type, uint32_t invert)
 {
 	/* Reset the controller */
 	p_acc->ACC_CR |= ACC_CR_SWRST;
@@ -81,65 +72,35 @@ void acc_init(Acc *p_acc, uint32_t select_plus, uint32_t select_minus,
 	while (p_acc->ACC_ISR & (uint32_t) ACC_ISR_MASK);
 }
 
-/**
- * \brief Enable the ACC.
- * \param p_acc Pointer to ACC registers set instance.
- */
 void acc_enable(Acc *p_acc)
 {
 	p_acc->ACC_MR |= ACC_MR_ACEN_EN;
 }
 
-/**
- * \brief Disable the ACC.
- * \param p_acc Pointer to ACC registers set instance.
- */
 void acc_disable(Acc *p_acc)
 {
 	p_acc->ACC_MR &= ~ACC_MR_ACEN_EN;
 }
 
-/**
- * \brief Reset the ACC.
- * \param p_acc Pointer to ACC registers set instance.
- */
 void acc_reset(Acc *p_acc)
 {
 	p_acc->ACC_CR = ACC_CR_SWRST;
 }
 
-/**
- * \brief Set the input source.
- * \param p_acc Pointer to ACC registers set instance.
- * \param select_minus Selection for minus comparator input.
- * \param select_plus Selection for plus comparator input.
- */
-void acc_set_input(Acc *p_acc, uint32_t select_minus,
-		uint32_t select_plus)
+void acc_set_input(Acc *p_acc, uint32_t select_minus, uint32_t select_plus)
 {
 	p_acc->ACC_MR &= ~(ACC_MR_SELMINUS_Msk & ACC_MR_SELPLUS_Msk);
 	p_acc->ACC_MR |= select_plus | select_minus;
 }
 
-/**
- * \brief Set the output of the ACC.
- * \param p_acc Pointer to ACC registers set instance.
- * \param invert Invert comparator output, 0 for disable, 1 for enable.
- * \param fault_enable Fault enable, 0 for disable, 1 for enable.
- * \param fault_source Selection of fault source, 0 for CF, 1 for output.
- */
-void acc_set_output(Acc *p_acc, uint32_t invert, uint32_t fault_enable, uint32_t fault_source)
+void acc_set_output(Acc *p_acc, uint32_t invert, uint32_t fault_enable,
+                    uint32_t fault_source)
 {
 	p_acc->ACC_MR &= ~(ACC_MR_INV_EN & ACC_MR_FE_EN & ACC_MR_SELFS_OUTPUT);
 
 	p_acc->ACC_MR |= invert | fault_source | fault_enable;
 }
 
-/**
- * \brief Get the comparison result.
- * \param p_acc Pointer to ACC registers set instance.
- * \return result of the comparison, 0 for inn>inp, 1 for inp>inn.
- */
 uint32_t acc_get_comparison_result(Acc *p_acc)
 {
 	uint32_t temp = p_acc->ACC_MR;
@@ -160,39 +121,21 @@ uint32_t acc_get_comparison_result(Acc *p_acc)
 	}
 }
 
-/**
- * \brief Enable the interrupt.
- * \param p_acc Pointer to ACC registers set instance.
- */
 void acc_enable_interrupt(Acc *p_acc)
 {
 	p_acc->ACC_IER = ACC_IER_CE;
 }
 
-/**
- * \brief Disable the interrupt.
- * \param p_acc Pointer to ACC registers set instance.
- */
 void acc_disable_interrupt(Acc *p_acc)
 {
 	p_acc->ACC_IDR = ACC_IDR_CE;
 }
 
-/**
- * \brief Get the interrupt status.
- * \param p_acc Pointer to ACC registers set instance.
- * \return status of interrupt.
- */
 uint32_t acc_get_interrupt_status(Acc *p_acc)
 {
 	return p_acc->ACC_ISR;
 }
 
-/**
- * \brief Set write protect of ACC registers.
- * \param p_acc Pointer to ACC registers set instance.
- * \param enable 1 to enable, 0 to disable.
- */
 void acc_set_write_protect(Acc *p_acc, uint32_t enable)
 {
 	if (enable)
@@ -201,12 +144,6 @@ void acc_set_write_protect(Acc *p_acc, uint32_t enable)
 		p_acc->ACC_WPMR = ACC_WPMR_WPKEY_PASSWD;
 }
 
-/**
- * \brief Return write protect status.
- *
- * \retval 0 No write protection error.
- * \retval 1 Write protection error.
- */
 uint32_t acc_get_write_protect_status(Acc *p_acc)
 {
 	return p_acc->ACC_WPSR & ACC_WPSR_WPVS;
