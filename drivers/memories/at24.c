7dadcd7d890f75471494ca94bda775fabc0d561a
Didier Fronek <didier.fronek@atmel.com>
2015-11-27 09:02:48 +0100

    drivers/memories/at24: Update at24 driver
    Add async mode for SAMA5D4
    Add mutex and callback

--- drivers/memories/at24.c
+++ drivers/memories/at24.c
@@ -36,11 +36,13 @@
 #include "intmath.h"
 #include "timer.h"
 #include "trace.h"
+
 #include "memories/at24.h"
 #include "peripherals/pio.h"
 #include "peripherals/pmc.h"
 #include "peripherals/twid.h"
 #include "peripherals/twi.h"
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -50,40 +52,43 @@
  *         Local functions
  *----------------------------------------------------------------------------*/
 
-static uint32_t _at24_read(struct _at24* at24, uint8_t* buffer, uint32_t len)
+void at24_free_mutex(struct _twi_desc* twi, void* arg)
+{
+	twid_finish_transfert(twi);
+	if(arg)
+		mutex_free((mutex_t*)(arg));
+}
+
+static uint32_t _at24_read(struct _at24* at24, uint8_t* buffer, uint32_t len, mutex_t* mutex)
 {
 	struct _buffer in = {
 		.data = buffer,
 		.size = len
 	};
-	return twid_transfert(at24->twid, &in, 0,
-			      twid_finish_transfert_callback, 0);
+	return twid_transfert(at24->twid, &in, NULL, at24_free_mutex, (void*)mutex);
 }
 
-static uint32_t _at24_write(struct _at24* at24, const uint8_t* buffer,
-			    uint32_t len)
+static uint32_t _at24_write(struct _at24* at24, const uint8_t* buffer, uint32_t len, mutex_t* mutex)
 {
 	struct _buffer out = {
 		.data = (uint8_t*)buffer,
 		.size = len
 	};
-	return twid_transfert(at24->twid, 0, &out,
-			      twid_finish_transfert_callback, 0);
+	return twid_transfert(at24->twid, NULL, &out, at24_free_mutex, (void*)mutex);
 }
 
 //------------------------------------------------------------------------------
 ///        Exported functions
 //------------------------------------------------------------------------------
 
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// Function to read the Serial Number from a AT24MAC402 EEPROM
-//
-// add: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
-// pSerNumbr: a pointer to 0x0F bytes of memory
-//
+/* Function to read the Serial Number from a AT24MAC402 EEPROM
+ *
+ * add: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
+ * pSerNumbr: a pointer to 0x0F bytes of memory
+ */
 uint8_t at24_get_serial_number(struct _at24* at24)
 {
+	mutex_t mutex = 0;
 	uint8_t status = TWID_SUCCESS;
 	uint8_t dummy = 0x0;
 
@@ -92,60 +97,57 @@ uint8_t at24_get_serial_number(struct _at24* at24)
 	at24->twid->isize = 1;
 
 	// Tell the EEPROM where we would like to read from
-	status = _at24_read(at24, &dummy, 1); // Location of the serial number
-	if (status) {
+	status = _at24_read(at24, &dummy, 1, NULL); // Location of the serial number
+	if (status)
 		return status;
-	}
-
 	at24->twid->iaddr = 0;
 	at24->twid->isize = 0;
-
 	/* // Now read bytes from that memory address */
-	return _at24_read(at24, at24->serial_number,
-			  sizeof(at24->serial_number));
+	mutex_lock(&mutex);
+	status = _at24_read(at24, at24->serial_number, sizeof(at24->serial_number), &mutex);
+	while (mutex_is_locked(&mutex)) ;
+
+	return status;
 }
 
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// Function to read the MAC address from a AT24MAC402 EEPROM
-//
-// add: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
-// pMacAddr: a pointer to 6 bytes of memory
-//
+
+/* Function to read the MAC address from a AT24MAC402 EEPROM
+ *
+ * add: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
+ * pMacAddr: a pointer to 6 bytes of memory
+ */
 uint8_t at24_get_mac_address(struct _at24* at24)
 {
+	mutex_t mutex = 0;
 	uint8_t status = TWID_SUCCESS;
 	uint8_t dummy = 0x0;
 
 	at24->twid->slave_addr = at24->sn_addr;
 	at24->twid->iaddr = at24->eui_offset;
 	at24->twid->isize = 1;
-
 	// Tell the EEPROM where we would like to read from
-	status = _at24_read(at24, &dummy, 1); // Location of the EUI-48
-	if (status) {
+	status = _at24_read(at24, &dummy, 1, NULL); // Location of the EUI-48
+	if (status)
 		return status;
-	}
-
 	at24->twid->iaddr = 0;
 	at24->twid->isize = 0;
-
-	return _at24_read(at24, at24->mac_addr_48,
-			  sizeof(at24->mac_addr_48));
+	mutex_lock(&mutex);
+	status = _at24_read(at24, at24->mac_addr_48, sizeof(at24->mac_addr_48), &mutex);
+	while (mutex_is_locked(&mutex)) ;
+	return status;
 }
 
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// Function to read the AT24MAC402 EEPROM
-//
-// BitA0ToA2: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
-// add: add to the data
-// Len: data to be read
-// pDataBuf: a pointer to memory buffer
-//
+/* Function to read the AT24MAC402 EEPROM
+ *
+ * BitA0ToA2: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
+ * add: add to the data
+ * Len: data to be read
+ * pDataBuf: a pointer to memory buffer
+ */
 uint8_t at24_read_eep(struct _at24* at24, uint8_t addr,
 		      uint8_t* data, uint8_t length)
 {
+	mutex_t mutex = 0;
 	uint8_t status = TWID_SUCCESS;
 	uint8_t dummy = 0xDE;
 
@@ -154,26 +156,26 @@ uint8_t at24_read_eep(struct _at24* at24, uint8_t addr,
 	at24->twid->isize = 1;
 
 	// Tell the EEPROM where we would like to read from
-	status = _at24_read(at24, &dummy, 1);
-	if (status) {
+	status = _at24_read(at24, &dummy, 1, NULL);
+	if (status)
 		return status;
-	}
-
-	return _at24_read(at24, data, length);
+	mutex_lock(&mutex);
+	status = _at24_read(at24, data, length, &mutex);
+	while (mutex_is_locked(&mutex)) ;
+	return status;
 }
 
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// Function to write to the AT24MAC402 EEPROM
-//
-// BitA0ToA2: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
-// add: add to the data
-// Len: data to be read
-// pDataBuf: a pointer to data to write in EEP
-//
+/* Function to write to the AT24MAC402 EEPROM
+ *
+ * BitA0ToA2: the 3-bit address assigned to the EEPROM (using the A0 A1 A2 pins)
+ * add: add to the data
+ * Len: data to be read
+ * pDataBuf: a pointer to data to write in EEP
+ */
 uint8_t at24_write_eep(struct _at24* at24, uint8_t addr,
 		       const uint8_t* data, uint8_t length)
 {
+	mutex_t mutex = 0;
 	uint8_t status = TWID_SUCCESS;
 	uint8_t dummy = 0xDE;
 	uint8_t page_size = at24->desc.page_size;
@@ -183,35 +185,33 @@ uint8_t at24_write_eep(struct _at24* at24, uint8_t addr,
 	at24->twid->isize = 1;
 
 	// Tell the EEPROM where we would like to read from
-	status = _at24_read(at24, &dummy, 1);
-	if (status) {
+	status = _at24_read(at24, &dummy, 1, NULL);
+	if (status)
 		return status;
-	}
 
 	while (length) {
 		/* Compute number of bytes to program in page */
 		uint8_t write_size;
 		write_size = min_u32(length, page_size - (addr % page_size));
 		at24->twid->iaddr = addr;
-
-		status |= _at24_write(at24, data, write_size);
-
+		mutex_lock(&mutex);
+		status |= _at24_write(at24, data, write_size, &mutex);
+		while (mutex_is_locked(&mutex)) ;
 		length -= write_size;
 		addr += write_size;
 		data += write_size;
-		// Wait at least 10 ms
-		timer_sleep(10);
+		timer_sleep(10); /* Wait at least 10 ms */
 	};
-
 	return status;
 }
 
+/* Function to configuer the twi AT24MAC402 EEPROM
+ * Return status always SUCCES
+ */
 uint8_t at24_configure(struct _at24* at24, struct _twi_desc* twid)
 {
 	uint8_t status = TWID_SUCCESS;
-
 	at24->twid = twid;
 	twid_configure(twid);
-
 	return status;
 }
