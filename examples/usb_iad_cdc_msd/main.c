14497887ff66b1f6724e4a2233c70b3d4f6e0632
Chen Yi <yi.chen@atmel.com>
2015-12-24 10:52:41 +0800

    examples/usb_iad_cdc_msd: Add usb cdc mass storage composite example.
    Signed-off-by: Chen Yi <yi.chen@atmel.com>

---
+++ examples/usb_iad_cdc_msd/main.c
@@ -0,0 +1,449 @@
+/* ----------------------------------------------------------------------------
+ *         SAM Software Package License
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2015, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ----------------------------------------------------------------------------
+ */
+/** \cond usb_iad_cdc_msd
+ * \page usb_iad_cdc_msd USB CDC(Serial)+MSD Example
+ *
+ * \section Purpose
+ *
+ * The USB CDCMSD Project will help you to get familiar with the
+ * USB Device Port(UDP)interface and also some of the other interfaces in
+ * SAMA5 microcontrollers. Also it can help you to be familiar with the USB
+ * Framework that is used for rapid development of USB-compliant class
+ * drivers such as USB Communication Device class (CDC), and how to combine
+ * two USB functions to a single CDCMSD device (such as CDC + MSD).
+ *
+ * \section Requirements
+ *
+ * This package can be used with some of Atmel evaluation kits that have UDP
+ * interface, depending on the functions included.
+ *
+ *  \section win_drv_update Windows Driver Update
+ *
+ * The composite device is generally supported by Microsoft windows, but some
+ * patches are needed for muti-interface functions such as CDC & Audio. The
+ * example composite devices are tested under windows XP (SP3). For CDC
+ * serial port, additional windows driver file (CompositeCDCSerial.inf) can
+ * be found at usb\\device\\composite\\drv.
+ *
+ * The following is alternate update to fix the composite device support
+ * on windows XP:
+ *
+ * \subsection install_win_sp3 Install Windows Service Pack 3 (SP3)
+ *
+ * All the fixes for USB generic driver are included in window XP service pack
+ * 3. It can be found at
+ * http://technet.microsoft.com/zh-cn/windows/bb794714(en-us).aspx .
+ *
+ * \subsection install_win_hotfix Install Windows Hot Fixes
+ *
+ * Two hot fixes are necessary for window to recognize the composite device
+ * correctly:
+ *
+ * -# http://support.microsoft.com/kb/814560
+ * -# http://support.microsoft.com/kb/918365
+ *
+ * \section Description
+ *
+ * The demo simulates a USB composite device that integrates USB CDC Serial
+ * RS232 Converter function and USB Disk function.
+ *
+ * When an EK running this program connected to a host (PC for example), with
+ * USB cable, host will notice the attachment of a USB device. No device
+ * driver offered for the device now.
+ *
+ * \section Usage
+ *
+ * -# Build the program and download it inside the evaluation board. Please
+ *    refer to the
+ *    <a href="http://www.atmel.com/dyn/resources/prod_documents/doc6421.pdf">
+ *    SAM-BA User Guide</a>, the
+ *    <a href="http://www.atmel.com/dyn/resources/prod_documents/doc6310.pdf">
+ *    GNU-Based Software Development</a> application note or to the
+ *    <a href="ftp://ftp.iar.se/WWWfiles/arm/Guides/EWARM_UserGuide.ENU.pdf">
+ *    IAR EWARM User Guide</a>, depending on your chosen solution.
+ * -# On the computer, open and configure a terminal application
+ *    (e.g. HyperTerminal on Microsoft Windows) with these settings:
+ *   - 115200 bauds
+ *   - 8 bits of data
+ *   - No parity
+ *   - 1 stop bit
+ *   - No flow control
+ * -# Start the application.
+ * -# In the terminal window, the following text should appear:
+ *     \code
+ *     -- USB CDCMSD Device Project xxx --
+ *     -- SAMxxxxx-xx
+ *     -- Compiled: xxx xx xxxx xx:xx:xx --
+ *     \endcode
+ * -# When connecting USB cable to windows, the LED blinks, and the host
+ *    reports a new USB device attachment.
+ * -# For the windows driver installation and the test functions, please
+ *      refer to "USB CDC serial converter" &
+ *      "USB Device Mass Storage Project".
+ * -# You can use the inf file
+ *    libraries\\usb\\device\\composite\\drv\\CompositeCDCSerial.inf
+ *    to install the CDC serial  port.
+ *
+ * \section References
+ *
+ * - usb_iad_cdc_msd/main.c
+ * - pio: Pin configurations and peripheral configure.
+ * - memories: Storage Media interface for MSD
+ * - usb: USB Device Framework, USB CDC driver and UDP interface driver
+ *    - \ref usbd_framework
+ *        - \ref usbd_api
+ *    - \ref usbd_composite "composite"
+ *       - \ref usbd_composite_drv
+ *    - \ref usbd_msd "massstorage"
+ *       - \ref usbd_msd_drv
+ *    - \ref usbd_cdc "cdc-serial"
+ *       - \ref usbd_cdc_serial_drv
+ * - projects:
+ *    - \ref usb_massstorage
+ *    - \ref usb_cdc_serial
+ */
+
+/**
+ * \file
+ *
+ * \section Purpose
+ *
+ * This file contains all the specific code for the
+ * usb_iad_cdc_msd project
+ */
+
+/*---------------------------------------------------------------------------
+ *         Headers
+ *---------------------------------------------------------------------------*/
+
+#include "board.h"
+#include "chip.h"
+#include "trace.h"
+
+#include "compiler.h"
+
+#include "cortex-a/mmu.h"
+#include "cortex-a/cp15.h"
+#include "misc/console.h"
+
+#include "peripherals/wdt.h"
+#include "peripherals/pio.h"
+
+
+#include "libstoragemedia/media.h"
+#include "libstoragemedia/media_private.h"
+#include "libstoragemedia/media_ramdisk.h"
+
+#include "usb/device/msd/msd_driver.h"
+#include "usb/device/msd/msd_lun.h"
+#include "usb/device/composite/cdc_msd_driver.h"
+#include "usb/device/cdc/cdcd_serial.h"
+#include "../usb_common/main_usb_common.h"
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/*---------------------------------------------------------------------------
+ *      Definitions
+ *---------------------------------------------------------------------------*/
+
+/** Size in bytes of the packet used for reading data from USB */
+#define DATAPACKETSIZE 1024
+
+/** Size in bytes of the buffer used for reading data from the USB & USART */
+#define DATABUFFERSIZE (DATAPACKETSIZE+2)
+
+/** Maximum number of LUNs which can be defined. */
+#define MAX_LUNS            1
+/** Media index for different disks */
+#define DRV_RAMDISK         0    /**< RAM disk */
+#define DRV_SDMMC           1    /**< SD card */
+#define DRV_NAND            2    /**< Nand flash */
+
+/** Size of one block in bytes. */
+#define BLOCK_SIZE          512
+
+/** Size of the MSD IO buffer in bytes (6K, more the better). */
+#define MSD_BUFFER_SIZE     (128*BLOCK_SIZE)
+
+/** Ramdisk size: 20K (WinXP can not format the disk if lower than 20K) */
+#define RAMDISK_SIZE        (8*1024*1024)
+
+/** Size of the reserved DDRAM (32M) */
+#define DDRAM_RESERVE_SIZE  (32*1024*1024)
+
+/** Size of the reserved Nand Flash (4M) */
+#define NF_RESERVE_SIZE     (4*1024*1024)
+
+/** Size of the managed Nand Flash (128M) */
+#define NF_MANAGED_SIZE     (128*1024*1024)
+
+/** Delay for MSD refresh (*4ms) */
+#define MSD_REFRESH_DELAY    250
+
+/** Delay for waiting DBGU input (*4ms) */
+#define INPUT_DELAY          (2*250)
+
+/*---------------------------------------------------------------------------
+ *      External variables
+ *---------------------------------------------------------------------------*/
+
+/** Descriptor list for the device to bring up */
+extern const USBDDriverDescriptors cdc_msdd_driver_descriptors;
+
+/*---------------------------------------------------------------------------
+ *      Internal variables
+ *---------------------------------------------------------------------------*/
+SECTION(".region_ddr")
+ALIGNED(BLOCK_SIZE)
+static uint8_t ramdisk_reserved[RAMDISK_SIZE];
+
+/** Buffer for storing incoming USB data. */
+SECTION(".region_ddr")
+ALIGNED(BLOCK_SIZE)
+static uint8_t usb_serial_buffer0[DATABUFFERSIZE];
+
+/** Serial port openned */
+static uint8_t is_serial_port_on = 0;
+
+/*- MSD */
+/** Available medias. */
+struct _media medias[MAX_LUNS];
+
+/** Device LUNs. */
+MSDLun luns[MAX_LUNS];
+
+/** LUN read/write buffer. */
+SECTION(".region_ddr")
+ALIGNED(BLOCK_SIZE)
+uint8_t msd_buffer[MSD_BUFFER_SIZE];
+
+/** Total data write to disk */
+uint32_t msd_write_total = 0;
+/** Delay for data write refresh */
+uint32_t msd_delay = MSD_REFRESH_DELAY;
+/** Delay TO event */
+uint8_t  msd_refresh = 0;
+
+
+/**
+ * Invoked when the configuration of the device changes. Parse used endpoints.
+ * \param cfgnum New configuration number.
+ */
+void usbd_driver_callbacks_configuration_changed(uint8_t cfgnum)
+{
+	cdc_msd_driver_configuration_changed_handler(cfgnum);
+}
+
+/**
+ * Invoked when a new SETUP request is received from the host. Forwards the
+ * request to the Mass Storage device driver handler function.
+ * \param request  Pointer to a USBGenericRequest instance.
+ */
+void usbd_callbacks_request_received(const USBGenericRequest *request)
+{
+	cdc_msd_driver_request_handler(request);
+}
+
+/*----------------------------------------------------------------------------
+ *         Callbacks
+ *----------------------------------------------------------------------------*/
+
+/**
+ * Invoked when the MSD finish a READ/WRITE.
+ * \param flow_direction 1 - device to host (READ10)
+ *                      0 - host to device (WRITE10)
+ * \param data_length Length of data transferred in bytes.
+ * \param fifo_null_count Times that FIFO is NULL to wait
+ * \param fifo_full_count Times that FIFO is filled to wait
+ */
+static void msd_callbacks_data(uint8_t flow_direction,
+								uint32_t  data_length,
+								uint32_t  fifo_null_count,
+								uint32_t  fifo_full_count)
+{
+	fifo_null_count = fifo_null_count; /*dummy */
+	fifo_full_count = fifo_full_count; /*dummy */
+
+	if (!flow_direction)
+		msd_write_total += data_length;
+}
+
+
+/**
+ * Invoked when the CDC ControlLineState is changed
+ * \param DTR   New DTR value.
+ * \param RTS   New RTS value.
+ */
+void cdcd_serial_control_line_sate_changed(uint8_t DTR,
+		uint8_t RTS)
+{
+	is_serial_port_on = DTR;
+	RTS = RTS; /* dummy */
+}
+
+/**
+ * Invoked when the CDC LineCoding is requested to changed
+ * \param pLineCoding   Pointer to new LineCoding settings.
+ * \return USBRC_SUCCESS if ready to receive the line coding.
+ */
+uint8_t cdcd_serial_line_coding_is_to_change(CDCLineCoding *line_coding)
+{
+	line_coding = line_coding; /*dummy */
+	return USBD_STATUS_SUCCESS;
+}
+
+/*---------------------------------------------------------------------------
+ *         Internal functions
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Initialize DDRAM to assign ramdisk block
+ */
+static void ram_disk_init(void)
+{
+	trace_info("RamDisk @ %x, size %d\n\r", (DDR_CS_ADDR + DDRAM_RESERVE_SIZE),
+				RAMDISK_SIZE);
+
+	media_ramdisk_init(&(medias[DRV_RAMDISK]),
+			((uint32_t)&ramdisk_reserved) / BLOCK_SIZE,
+			RAMDISK_SIZE / BLOCK_SIZE,
+			BLOCK_SIZE);
+
+	lun_init(&(luns[DRV_RAMDISK]),
+				&(medias[DRV_RAMDISK]),
+				msd_buffer, MSD_BUFFER_SIZE,
+				0, 0, 0, 0,
+				msd_callbacks_data);
+}
+
+/**
+ * Initialize MSD Media & LUNs
+ */
+static void _memories_initialize(void)
+{
+	uint32_t i ;
+
+	/* Reset all LUNs */
+	for (i = 0; i < MAX_LUNS; i ++)
+		lun_init(&luns[i], 0, 0, 0, 0, 0, 0, 0, 0);
+
+	/* RAM disk initialize */
+	ram_disk_init();
+}
+
+/*---------------------------------------------------------------------------
+ *         Exported function
+ *---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------
+ *          Main
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Initializes drivers and start the USB CDCMSD device.
+ */
+int main(void)
+{
+	uint8_t usb_connected = 0, serial_on = 0;
+
+	/* Disable watchdog */
+	wdt_disable();
+
+	board_cfg_console();
+
+	/* Enable DDRAM */
+#ifndef VARIANT_DDRAM
+	board_cfg_ddram();
+#endif
+
+	printf("-- USB CDCMSD Device Project %s --\n\r", SOFTPACK_VERSION);
+	printf("-- %s\n\r", BOARD_NAME);
+	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+
+	/* If they are present, configure Vbus & Wake-up pins */
+	pio_reset_all_it();
+
+	/* If there is on board power, switch it off */
+	usb_power_configure();
+
+	/* ----- MSD Function Initialize */
+	/* Configure memories */
+	_memories_initialize();
+
+	/* USB CDCMSD driver initialization */
+	cdc_msd_driver_initialize(&cdc_msdd_driver_descriptors, luns, MAX_LUNS);
+
+	/* connect if needed */
+	usb_vbus_configure();
+
+	/* Driver loop */
+	while (1) {
+		/* Device is not configured */
+		if (usbd_get_state() < USBD_STATE_CONFIGURED) {
+			if (usb_connected) {
+				printf("-I- USB Disconnect/Suspend\n\r");
+				usb_connected = 0;
+
+				/* Serial port closed */
+				is_serial_port_on = 0;
+			}
+		} else {
+			if (usb_connected == 0) {
+				printf("-I- USB Connect\n\r");
+				usb_connected = 1;
+			}
+
+			if (!serial_on && is_serial_port_on) {
+				printf("-I- SerialPort ON\n\r");
+				/* Start receiving data on the USART */
+				/* Start receiving data on the USB */
+				cdcd_serial_read(usb_serial_buffer0, DATAPACKETSIZE, 0, 0);
+				serial_on = 1;
+			} else if (serial_on && !is_serial_port_on) {
+				printf("-I- SerialPort OFF\n\r");
+				serial_on = 0;
+			}
+			msd_function_state_machine();
+
+			if (msd_refresh) {
+				msd_refresh = 0;
+
+				if (msd_write_total < 50 * 1000) {
+					/* Flush Disk Media */
+				}
+
+				msd_write_total = 0;
+			}
+		}
+	}
+}
+/** \endcond */
---
+++ examples/usb_iad_cdc_msd/main_descriptors.c
@@ -0,0 +1,323 @@
+/* ----------------------------------------------------------------------------
+ *         SAM Software Package License
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2015, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ----------------------------------------------------------------------------
+ */
+/** \file */
+/** \addtogroup usbd_composite_cdcmsd
+ *@{
+ */
+/*------------------------------------------------------------------------------
+ *      Headers
+ *----------------------------------------------------------------------------*/
+
+#include "board.h"
+#include "usb/device/composite/cdc_msd_driver.h"
+#include "usb/device/cdc/cdcd_serial.h"
+#include "usb/device/msd/msd_function.h"
+
+/*-----------------------------------------------------------------------------
+ *         Definitions
+ *---------------------------------------------------------------------------*/
+
+/** Address of the CDC interrupt-in endpoint. */
+#define CDCD_Descriptors_NOTIFICATION0              3
+/** Address of the CDC bulk-in endpoint. */
+#define CDCD_Descriptors_DATAIN0                    2
+/** Address of the CDC bulk-out endpoint. */
+#define CDCD_Descriptors_DATAOUT0                   1
+/** Address of the Mass Storage bulk-out endpoint. */
+#define MSDD_Descriptors_BULKOUT                    4
+/** Address of the Mass Storage bulk-in endpoint. */
+#define MSDD_Descriptors_BULKIN                     5
+
+/** Device product ID. */
+#define CDCMSDDDriverDescriptors_PRODUCTID       0x6132
+/** Device vendor ID (Atmel). */
+#define CDCMSDDDriverDescriptors_VENDORID        0x03EB
+/** Device release number. */
+#define CDCMSDDDriverDescriptors_RELEASE         0x0100
+
+/*-----------------------------------------------------------------------------
+ *         Macros
+ *---------------------------------------------------------------------------*/
+
+/** Returns the minimum between two values. */
+#define MIN(a, b)       ((a < b) ? a : b)
+
+/*------------------------------------------------------------------------------
+ *         Internal variables
+ *----------------------------------------------------------------------------*/
+
+/** Standard USB device descriptor for the CDCMSD device driver */
+static const USBDeviceDescriptor deviceDescriptor = {
+
+	sizeof(USBDeviceDescriptor),
+	USBGenericDescriptor_DEVICE,
+	USBDeviceDescriptor_USB2_00,
+	0xEF, /* MI */
+	0x02,
+	0x01,
+	CHIP_USB_ENDPOINT_MAXPACKETSIZE(0),
+	CDCMSDDDriverDescriptors_VENDORID,
+	CDCMSDDDriverDescriptors_PRODUCTID,
+	CDCMSDDDriverDescriptors_RELEASE,
+	0, /* No string descriptor for manufacturer */
+	1, /* Index of product string descriptor is #1 */
+	0, /* No string descriptor for serial number */
+	1  /* Device has 1 possible configuration */
+};
+
+/** USB configuration descriptors for the CDCMSD device driver */
+static const CDCMSDDriverConfigurationDescriptors configurationDescriptorsFS = {
+
+	/* Standard configuration descriptor */
+	{
+		sizeof(USBConfigurationDescriptor),
+		USBGenericDescriptor_CONFIGURATION,
+		sizeof(CDCMSDDriverConfigurationDescriptors),
+		CDCMSDDriverDescriptors_NUMINTERFACE,
+		1, /* This is configuration #1 */
+		0, /* No string descriptor for this configuration */
+		BOARD_USB_BMATTRIBUTES,
+		USBConfigurationDescriptor_POWER(100)
+	},
+
+	/* CDC */
+	/* IAD for CDC/ACM port */
+	{
+		sizeof(USBInterfaceAssociationDescriptor),
+		USBGenericDescriptor_INTERFACEASSOCIATION,
+		CDCMSDDriverDescriptors_CDC_INTERFACE,
+		2,
+		CDCCommunicationInterfaceDescriptor_CLASS,
+		CDCCommunicationInterfaceDescriptor_ABSTRACTCONTROLMODEL,
+		CDCCommunicationInterfaceDescriptor_NOPROTOCOL,
+		0  /* No string descriptor for this interface */
+	},
+	/* Communication class interface standard descriptor */
+	{
+		sizeof(USBInterfaceDescriptor),
+		USBGenericDescriptor_INTERFACE,
+		CDCMSDDriverDescriptors_CDC_INTERFACE, /* This is interface #0 */
+		0, /* This is alternate setting #0 for this interface */
+		1, /* This interface uses 1 endpoint */
+		CDCCommunicationInterfaceDescriptor_CLASS,
+		CDCCommunicationInterfaceDescriptor_ABSTRACTCONTROLMODEL,
+		CDCCommunicationInterfaceDescriptor_NOPROTOCOL,
+		0  /* No string descriptor for this interface */
+	},
+	/* Class-specific header functional descriptor */
+	{
+		sizeof(CDCHeaderDescriptor),
+		CDCGenericDescriptor_INTERFACE,
+		CDCGenericDescriptor_HEADER,
+		CDCGenericDescriptor_CDC1_10
+	},
+	/* Class-specific call management functional descriptor */
+	{
+		sizeof(CDCCallManagementDescriptor),
+		CDCGenericDescriptor_INTERFACE,
+		CDCGenericDescriptor_CALLMANAGEMENT,
+		CDCCallManagementDescriptor_SELFCALLMANAGEMENT,
+		CDCMSDDriverDescriptors_CDC_INTERFACE + 1 /* No associated data interface */
+	},
+	/* Class-specific abstract control management functional descriptor */
+	{
+		sizeof(CDCAbstractControlManagementDescriptor),
+		CDCGenericDescriptor_INTERFACE,
+		CDCGenericDescriptor_ABSTRACTCONTROLMANAGEMENT,
+		CDCAbstractControlManagementDescriptor_LINE
+	},
+	/* Class-specific union functional descriptor with one slave interface */
+	{
+		sizeof(CDCUnionDescriptor),
+		CDCGenericDescriptor_INTERFACE,
+		CDCGenericDescriptor_UNION,
+		CDCMSDDriverDescriptors_CDC_INTERFACE, /* Number of master interface is #0 */
+		CDCMSDDriverDescriptors_CDC_INTERFACE + 1 /* First slave interface is #1 */
+	},
+	/* Notification endpoint standard descriptor */
+	{
+		sizeof(USBEndpointDescriptor),
+		USBGenericDescriptor_ENDPOINT,
+		USBEndpointDescriptor_ADDRESS(USBEndpointDescriptor_IN,
+		CDCD_Descriptors_NOTIFICATION0),
+		USBEndpointDescriptor_INTERRUPT,
+		MIN(CHIP_USB_ENDPOINT_MAXPACKETSIZE(CDCD_Descriptors_NOTIFICATION0),
+		CDCDSerialPort_INTERRUPT_MAXPACKETSIZE),
+		CDCDSerialPort_INTERRUPT_INTERVAL_FS
+	},
+	/* Data class interface standard descriptor */
+	{
+		sizeof(USBInterfaceDescriptor),
+		USBGenericDescriptor_INTERFACE,
+		CDCMSDDriverDescriptors_CDC_INTERFACE + 1, /* This is interface #1 */
+		0, /* This is alternate setting #0 for this interface */
+		2, /* This interface uses 2 endpoints */
+		CDCDataInterfaceDescriptor_CLASS,
+		CDCDataInterfaceDescriptor_SUBCLASS,
+		CDCDataInterfaceDescriptor_NOPROTOCOL,
+		0  /* No string descriptor for this interface */
+	},
+	/* Bulk-OUT endpoint standard descriptor */
+	{
+		sizeof(USBEndpointDescriptor),
+		USBGenericDescriptor_ENDPOINT,
+		USBEndpointDescriptor_ADDRESS(USBEndpointDescriptor_OUT,
+		CDCD_Descriptors_DATAOUT0),
+		USBEndpointDescriptor_BULK,
+		MIN(CHIP_USB_ENDPOINT_MAXPACKETSIZE(CDCD_Descriptors_DATAOUT0),
+		USBEndpointDescriptor_MAXBULKSIZE_FS),
+		0 /* Must be 0 for full-speed bulk endpoints */
+	},
+	/* Bulk-IN endpoint descriptor */
+	{
+		sizeof(USBEndpointDescriptor),
+		USBGenericDescriptor_ENDPOINT,
+		USBEndpointDescriptor_ADDRESS(USBEndpointDescriptor_IN,
+		CDCD_Descriptors_DATAIN0),
+		USBEndpointDescriptor_BULK,
+		MIN(CHIP_USB_ENDPOINT_MAXPACKETSIZE(CDCD_Descriptors_DATAIN0),
+		USBEndpointDescriptor_MAXBULKSIZE_FS),
+		0 /* Must be 0 for full-speed bulk endpoints */
+	},
+
+	/* Mass Storage interface descriptor. */
+	{
+		sizeof(USBInterfaceDescriptor),
+		USBGenericDescriptor_INTERFACE,
+		CDCMSDDriverDescriptors_MSD_INTERFACE,
+		0, /* This is alternate setting #0. */
+		2, /* Interface uses two endpoints. */
+		MSInterfaceDescriptor_CLASS,
+		MSInterfaceDescriptor_SCSI,
+		MSInterfaceDescriptor_BULKONLY,
+		0 /* No string descriptor for interface. */
+	},
+	/* Bulk-OUT endpoint descriptor */
+	{
+		sizeof(USBEndpointDescriptor),
+		USBGenericDescriptor_ENDPOINT,
+		USBEndpointDescriptor_ADDRESS(
+			USBEndpointDescriptor_OUT,
+			MSDD_Descriptors_BULKOUT),
+		USBEndpointDescriptor_BULK,
+		MIN(CHIP_USB_ENDPOINT_MAXPACKETSIZE(MSDD_Descriptors_BULKOUT),
+		USBEndpointDescriptor_MAXBULKSIZE_FS),
+		0 /* No string descriptor for endpoint. */
+	},
+	/* Bulk-IN endpoint descriptor */
+	{
+		sizeof(USBEndpointDescriptor),
+		USBGenericDescriptor_ENDPOINT,
+		USBEndpointDescriptor_ADDRESS(
+			USBEndpointDescriptor_IN,
+			MSDD_Descriptors_BULKIN),
+		USBEndpointDescriptor_BULK,
+		MIN(CHIP_USB_ENDPOINT_MAXPACKETSIZE(MSDD_Descriptors_BULKIN),
+		USBEndpointDescriptor_MAXBULKSIZE_FS),
+		0 /* No string descriptor for endpoint. */
+	}
+
+};
+
+/** String descriptor with the supported languages. */
+static const unsigned char languageIdDescriptor[] = {
+
+	USBStringDescriptor_LENGTH(1),
+	USBGenericDescriptor_STRING,
+	USBStringDescriptor_ENGLISH_US
+};
+
+/** Manufacturer name. */
+static const unsigned char manufacturerDescriptor[] = {
+
+	USBStringDescriptor_LENGTH(5),
+	USBGenericDescriptor_STRING,
+	USBStringDescriptor_UNICODE('A'),
+	USBStringDescriptor_UNICODE('t'),
+	USBStringDescriptor_UNICODE('m'),
+	USBStringDescriptor_UNICODE('e'),
+	USBStringDescriptor_UNICODE('l')
+};
+
+/** Product name. */
+static const unsigned char productDescriptor[] = {
+
+	USBStringDescriptor_LENGTH(14),
+	USBGenericDescriptor_STRING,
+	USBStringDescriptor_UNICODE('C'),
+	USBStringDescriptor_UNICODE('o'),
+	USBStringDescriptor_UNICODE('m'),
+	USBStringDescriptor_UNICODE('p'),
+	USBStringDescriptor_UNICODE('o'),
+	USBStringDescriptor_UNICODE('s'),
+	USBStringDescriptor_UNICODE('i'),
+	USBStringDescriptor_UNICODE('t'),
+	USBStringDescriptor_UNICODE('e'),
+	USBStringDescriptor_UNICODE(' '),
+	USBStringDescriptor_UNICODE('D'),
+	USBStringDescriptor_UNICODE('e'),
+	USBStringDescriptor_UNICODE('m'),
+	USBStringDescriptor_UNICODE('o')
+};
+
+/** Product serial number. */
+static const unsigned char serialNumberDescriptor[] = {
+
+	USBStringDescriptor_LENGTH(4),
+	USBGenericDescriptor_STRING,
+	USBStringDescriptor_UNICODE('0'),
+	USBStringDescriptor_UNICODE('1'),
+	USBStringDescriptor_UNICODE('2'),
+	USBStringDescriptor_UNICODE('3')
+};
+
+/** Array of pointers to the four string descriptors. */
+static const unsigned char *stringDescriptors[] = {
+
+	languageIdDescriptor,
+	manufacturerDescriptor,
+	productDescriptor,
+	serialNumberDescriptor,
+};
+
+/*----------------------------------------------------------------------------
+ *         Exported variables
+ *----------------------------------------------------------------------------*/
+
+/** List of descriptors required by an USB audio speaker device driver. */
+const USBDDriverDescriptors cdc_msdd_driver_descriptors = {
+
+	&deviceDescriptor,
+	(const USBConfigurationDescriptor *) &configurationDescriptorsFS,
+	0, 0, 0, 0, 0, 0,
+	stringDescriptors,
+	4 /* Number of string descriptors */
+};
+/**@}*/
+
