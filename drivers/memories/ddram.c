33f6b042111251e1d219a63f6defaaaaaa5a52a3
Loic Lefort <loic.lefort@atmel.com>
2016-01-04 11:07:22 +0100

    drivers/memories/ddram: Fix indentation
    Signed-off-by: Loic Lefort <loic.lefort@atmel.com>

--- drivers/memories/ddram.c
+++ drivers/memories/ddram.c
@@ -62,42 +62,43 @@ static void _init_mt41k128m16(struct _mpddrc_desc* desc)
 
 	desc->type = MPDDRC_TYPE_DDR3;
 
-	desc->io_calibr = MPDDRC_IO_CALIBR_RDIV_RZQ_60 |
-		MPDDRC_IO_CALIBR_TZQIO(100);
-
 	desc->mode = MPDDRC_MD_MD_DDR3_SDRAM;
 
 	desc->data_path = MPDDRC_RD_DATA_PATH_SHIFT_SAMPLING_SHIFT_TWO_CYCLES;
 
 	desc->control = MPDDRC_CR_NC_10_COL_BITS
-		| MPDDRC_CR_NR_14_ROW_BITS
-		| MPDDRC_CR_CAS_DDR_CAS5
-		| MPDDRC_CR_DIS_DLL
-		| MPDDRC_CR_NB_8_BANKS
-		| MPDDRC_CR_DIC_DS_DDR2_WEAKSTRENGTH_DDR3_RZQ7
-		| MPDDRC_CR_DECOD_INTERLEAVED
-		| MPDDRC_CR_UNAL_SUPPORTED;
+	              | MPDDRC_CR_NR_14_ROW_BITS
+	              | MPDDRC_CR_CAS_DDR_CAS5
+	              | MPDDRC_CR_DIS_DLL
+	              | MPDDRC_CR_NB_8_BANKS
+	              | MPDDRC_CR_DIC_DS_DDR2_WEAKSTRENGTH_DDR3_RZQ7
+	              | MPDDRC_CR_DECOD_INTERLEAVED
+	              | MPDDRC_CR_UNAL_SUPPORTED;
+
+	desc->io_calibr = MPDDRC_IO_CALIBR_RDIV_RZQ_60
+	                | MPDDRC_IO_CALIBR_TZQIO(100);
 
 	/* timings */
-	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(35, mck))  // 35ns
-		| MPDDRC_TPR0_TRCD(NS2CYCLES(14, mck))         // 13.75ns
-		| MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))          // 15ns
-		| MPDDRC_TPR0_TRC(NS2CYCLES(49, mck))          // 48.75ns
-		| MPDDRC_TPR0_TRP(NS2CYCLES(14, mck))          // 13.75ns
-		| MPDDRC_TPR0_TRRD(4)                          // greater of 4CK or 6ns
-		| MPDDRC_TPR0_TWTR(4)                          // greater of 4CK or 7.5ns
-		| MPDDRC_TPR0_TMRD(4);                         // min = 4CK
 
-	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(160, mck)) // 160ns
-		| MPDDRC_TPR1_TXSNR(NS2CYCLES(170, mck))       // tRFC+10ns
-		| MPDDRC_TPR1_TXSRD(0)
-		| MPDDRC_TPR1_TXP(10);                         // greater of 10CK or or 24ns
+	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(35, mck)) // 35ns
+	           | MPDDRC_TPR0_TRCD(NS2CYCLES(14, mck)) // 13.75ns
+	           | MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR0_TRC(NS2CYCLES(49, mck))  // 48.75ns
+	           | MPDDRC_TPR0_TRP(NS2CYCLES(14, mck))  // 13.75ns
+	           | MPDDRC_TPR0_TRRD(4)                  // greater of 4CK or 6ns
+	           | MPDDRC_TPR0_TWTR(4)                  // greater of 4CK or 7.5ns
+	           | MPDDRC_TPR0_TMRD(4);                 // min = 4CK
+
+	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(160, mck))  // 160ns
+	           | MPDDRC_TPR1_TXSNR(NS2CYCLES(170, mck)) // tRFC+10ns
+	           | MPDDRC_TPR1_TXSRD(0)
+	           | MPDDRC_TPR1_TXP(10);                   // greater of 10CK or or 24ns
 
 	desc->tpr2 = MPDDRC_TPR2_TXARD(0)
-		| MPDDRC_TPR2_TXARDS(0)
-		| MPDDRC_TPR2_TRPA(0)
-		| MPDDRC_TPR2_TRTP(4)                          // greater of 4CK or 7.5ns
-		| MPDDRC_TPR2_TFAW(NS2CYCLES(40, mck));        // 40ns
+	           | MPDDRC_TPR2_TXARDS(0)
+	           | MPDDRC_TPR2_TRPA(0)
+	           | MPDDRC_TPR2_TRTP(4)                   // greater of 4CK or 7.5ns
+	           | MPDDRC_TPR2_TFAW(NS2CYCLES(40, mck)); // 40ns
 
 	desc->bank = 8192;
 }
@@ -108,42 +109,43 @@ static void _init_edf8164a3ma(struct _mpddrc_desc* desc)
 
 	desc->type = MPDDRC_TYPE_LPDDR3;
 
-	desc->io_calibr = MPDDRC_IO_CALIBR_RDIV_RZQ_60;
-
 	desc->mode = MPDDRC_MD_MD_LPDDR3_SDRAM ;
 
 	desc->data_path = MPDDRC_RD_DATA_PATH_SHIFT_SAMPLING_SHIFT_TWO_CYCLES;
 
 	desc->control = MPDDRC_CR_NC_10_COL_BITS
-		| MPDDRC_CR_NR_14_ROW_BITS
-		| MPDDRC_CR_CAS_DDR_CAS3
-		| MPDDRC_CR_DLL_RESET_DISABLED
-		| MPDDRC_CR_NB_8_BANKS
-		| MPDDRC_CR_DIC_DS_DDR2_NORMALSTRENGTH_DDR3_RZQ6
-		| MPDDRC_CR_NDQS_ENABLED
-		| MPDDRC_CR_DECOD_SEQUENTIAL
-		| MPDDRC_CR_UNAL_SUPPORTED;
+	              | MPDDRC_CR_NR_14_ROW_BITS
+	              | MPDDRC_CR_CAS_DDR_CAS3
+	              | MPDDRC_CR_DLL_RESET_DISABLED
+	              | MPDDRC_CR_NB_8_BANKS
+	              | MPDDRC_CR_DIC_DS_DDR2_NORMALSTRENGTH_DDR3_RZQ6
+	              | MPDDRC_CR_NDQS_ENABLED
+	              | MPDDRC_CR_DECOD_SEQUENTIAL
+	              | MPDDRC_CR_UNAL_SUPPORTED;
+
+	desc->io_calibr = MPDDRC_IO_CALIBR_RDIV_RZQ_60;
 
 	/* timings */
-	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(42, mck))        // 42ns
-		| MPDDRC_TPR0_TRCD(MAX(NS2CYCLES(18, mck), 3))   // max(18ns | 3ck)
-		| MPDDRC_TPR0_TWR(MAX(NS2CYCLES(15, mck), 3))    // max(15ns | 3ck)
-		| MPDDRC_TPR0_TRC(NS2CYCLES(55, mck))            // 55ns
-		| MPDDRC_TPR0_TRP(MAX(NS2CYCLES(8, mck), 4))     // max(7.5ns, 4ck)
-		| MPDDRC_TPR0_TRRD(MAX(NS2CYCLES(10, mck), 2))   // max(10ns, 2ck)
-		| MPDDRC_TPR0_TWTR(MAX(NS2CYCLES(8, mck), 4))    // max(7.5ns, 4ck)
-		| MPDDRC_TPR0_TMRD(MAX(NS2CYCLES(14, mck), 10)); // max(14ns, 10ck)
 
-	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(130, mck))    // 130ns
-		| MPDDRC_TPR1_TXSNR(NS2CYCLES(140, mck))      // 140ns
-		| MPDDRC_TPR1_TXSRD(NS2CYCLES(140, mck))      // max(tRFCab + 10ns, 2ck)
-		| MPDDRC_TPR1_TXP(MAX(NS2CYCLES(8, mck), 2)); // max(7.5ns, 2ck)
+	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(42, mck))           // 42ns
+	           | MPDDRC_TPR0_TRCD(MAX(NS2CYCLES(18, mck), 3))   // max(18ns | 3ck)
+	           | MPDDRC_TPR0_TWR(MAX(NS2CYCLES(15, mck), 3))    // max(15ns | 3ck)
+	           | MPDDRC_TPR0_TRC(NS2CYCLES(55, mck))            // 55ns
+	           | MPDDRC_TPR0_TRP(MAX(NS2CYCLES(8, mck), 4))     // max(7.5ns, 4ck)
+	           | MPDDRC_TPR0_TRRD(MAX(NS2CYCLES(10, mck), 2))   // max(10ns, 2ck)
+	           | MPDDRC_TPR0_TWTR(MAX(NS2CYCLES(8, mck), 4))    // max(7.5ns, 4ck)
+	           | MPDDRC_TPR0_TMRD(MAX(NS2CYCLES(14, mck), 10)); // max(14ns, 10ck)
+
+	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(130, mck))       // 130ns
+	           | MPDDRC_TPR1_TXSNR(NS2CYCLES(140, mck))      // 140ns
+	           | MPDDRC_TPR1_TXSRD(NS2CYCLES(140, mck))      // max(tRFCab + 10ns, 2ck)
+	           | MPDDRC_TPR1_TXP(MAX(NS2CYCLES(8, mck), 2)); // max(7.5ns, 2ck)
 
 	desc->tpr2 = MPDDRC_TPR2_TXARD(0)
-		| MPDDRC_TPR2_TXARDS(0)
-		| MPDDRC_TPR2_TRPA(0)
-		| MPDDRC_TPR2_TRTP(MAX(NS2CYCLES(8, mck), 4))   // max(7.5ns, 4ck)
-		| MPDDRC_TPR2_TFAW(MAX(NS2CYCLES(50, mck), 8)); // max(50ns, 8ck)
+	           | MPDDRC_TPR2_TXARDS(0)
+	           | MPDDRC_TPR2_TRPA(0)
+	           | MPDDRC_TPR2_TRTP(MAX(NS2CYCLES(8, mck), 4))   // max(7.5ns, 4ck)
+	           | MPDDRC_TPR2_TFAW(MAX(NS2CYCLES(50, mck), 8)); // max(50ns, 8ck)
 
 	desc->bank = 8192;
 }
@@ -154,48 +156,52 @@ static void _init_mt47h128m8cf(struct _mpddrc_desc* desc)
 	uint32_t mck = pmc_get_master_clock() / 1000000;
 
 	desc->type = MPDDRC_TYPE_DDR2;
+
 	/* DBW = 0 (32 bits bus wide);
 	 * Memory Device = 6 = DDR2-SDRAM = 0x00000006 */
-	desc->mode = MPDDRC_MD_MD_DDR2_SDRAM | MPDDRC_MD_DBW_DBW_32_BITS;
+	desc->mode = MPDDRC_MD_MD_DDR2_SDRAM
+	           | MPDDRC_MD_DBW_DBW_32_BITS;
 
 	desc->data_path = MPDDRC_RD_DATA_PATH_SHIFT_SAMPLING_SHIFT_ONE_CYCLE;
 
-	desc->control = MPDDRC_CR_NR_14_ROW_BITS |
-		MPDDRC_CR_NC_10_COL_BITS |
-		MPDDRC_CR_CAS_DDR_CAS3 |
-		MPDDRC_CR_DLL_RESET_DISABLED |
-		MPDDRC_CR_DQMS_NOT_SHARED |
-		MPDDRC_CR_ENRDM_OFF |
-		MPDDRC_CR_NB_8_BANKS |
-		MPDDRC_CR_NDQS_DISABLED |
-		MPDDRC_CR_UNAL_SUPPORTED | MPDDRC_CR_OCD_DDR2_EXITCALIB;
+	desc->control = MPDDRC_CR_NR_14_ROW_BITS
+	              | MPDDRC_CR_NC_10_COL_BITS
+	              | MPDDRC_CR_CAS_DDR_CAS3
+	              | MPDDRC_CR_DLL_RESET_DISABLED
+	              | MPDDRC_CR_DQMS_NOT_SHARED
+	              | MPDDRC_CR_ENRDM_OFF
+	              | MPDDRC_CR_NB_8_BANKS
+	              | MPDDRC_CR_NDQS_DISABLED
+	              | MPDDRC_CR_UNAL_SUPPORTED
+	              | MPDDRC_CR_OCD_DDR2_EXITCALIB;
 
 	desc->io_calibr = MPDDRC_IO_CALIBR_CALCODEP(7)
-		| MPDDRC_IO_CALIBR_CALCODEN(8)
-		| MPDDRC_IO_CALIBR_RDIV_RZQ_60
-		| MPDDRC_IO_CALIBR_TZQIO(5)
-		| MPDDRC_IO_CALIBR_EN_CALIB_ENABLE_CALIBRATION;
+	                | MPDDRC_IO_CALIBR_CALCODEN(8)
+	                | MPDDRC_IO_CALIBR_RDIV_RZQ_60
+	                | MPDDRC_IO_CALIBR_TZQIO(5)
+	                | MPDDRC_IO_CALIBR_EN_CALIB_ENABLE_CALIBRATION;
 
 	/* timings */
-	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(40, mck))  // 40ns
-		| MPDDRC_TPR0_TRCD(NS2CYCLES(13, mck))         // 12.5ns
-		| MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))          // 15ns
-		| MPDDRC_TPR0_TRC(NS2CYCLES(55, mck))          // 55ns
-		| MPDDRC_TPR0_TRP(NS2CYCLES(13, mck))          // 12.5ns
-		| MPDDRC_TPR0_TRRD(NS2CYCLES(8, mck))          // 7.5ns
-		| MPDDRC_TPR0_TWTR(NS2CYCLES(10, mck))         // 10ns
-		| MPDDRC_TPR0_TMRD(2);                         // 2 clock cycles
 
-	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(128, mck)) // 127.5ns
-		| MPDDRC_TPR1_TXSNR(NS2CYCLES(138, mck))       // (tRFC)+10 = 128.5ns
-		| MPDDRC_TPR1_TXSRD(200)                       // 200ck
-		| MPDDRC_TPR1_TXP(2);                          // 2ck
+	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(40, mck)) // 40ns
+	           | MPDDRC_TPR0_TRCD(NS2CYCLES(13, mck)) // 12.5ns
+	           | MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR0_TRC(NS2CYCLES(55, mck))  // 55ns
+	           | MPDDRC_TPR0_TRP(NS2CYCLES(13, mck))  // 12.5ns
+	           | MPDDRC_TPR0_TRRD(NS2CYCLES(8, mck))  // 7.5ns
+	           | MPDDRC_TPR0_TWTR(NS2CYCLES(10, mck)) // 10ns
+	           | MPDDRC_TPR0_TMRD(2);                 // 2 clock cycles
+
+	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(128, mck))  // 127.5ns
+	           | MPDDRC_TPR1_TXSNR(NS2CYCLES(138, mck)) // (tRFC)+10 = 128.5ns
+	           | MPDDRC_TPR1_TXSRD(200)                 // 200ck
+	           | MPDDRC_TPR1_TXP(2);                    // 2ck
 
 	desc->tpr2 = MPDDRC_TPR2_TXARD(8)                  // 8ck
-		| MPDDRC_TPR2_TXARDS(2)                        // 2ck
-		| MPDDRC_TPR2_TRPA(NS2CYCLES(15, mck))         // 15ns
-		| MPDDRC_TPR2_TRTP(NS2CYCLES(8, mck))          // 7.5ns
-		| MPDDRC_TPR2_TFAW(NS2CYCLES(35, mck));        // 35ns
+	           | MPDDRC_TPR2_TXARDS(2)                 // 2ck
+	           | MPDDRC_TPR2_TRPA(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR2_TRTP(NS2CYCLES(8, mck))   // 7.5ns
+	           | MPDDRC_TPR2_TFAW(NS2CYCLES(35, mck)); // 35ns
 
 	desc->bank = 8192;
 }
@@ -205,48 +211,52 @@ static void _init_mt47h128m16(struct _mpddrc_desc* desc)
 	uint32_t mck = pmc_get_master_clock() / 1000000;
 
 	desc->type = MPDDRC_TYPE_DDR2;
+
 	/* DBW = 0 (32 bits bus wide);
 	 * Memory Device = 6 = DDR2-SDRAM = 0x00000006 */
-	desc->mode = MPDDRC_MD_MD_DDR2_SDRAM | MPDDRC_MD_DBW_DBW_32_BITS;
+	desc->mode = MPDDRC_MD_MD_DDR2_SDRAM
+	           | MPDDRC_MD_DBW_DBW_32_BITS;
 
 	desc->data_path = MPDDRC_RD_DATA_PATH_SHIFT_SAMPLING_SHIFT_ONE_CYCLE;
 
-	desc->io_calibr = MPDDRC_IO_CALIBR_CALCODEP(7)
-		| MPDDRC_IO_CALIBR_CALCODEN(8)
-		| MPDDRC_IO_CALIBR_RDIV_RZQ_60
-		| MPDDRC_IO_CALIBR_TZQIO(5)
-		| MPDDRC_IO_CALIBR_EN_CALIB_ENABLE_CALIBRATION;
+	desc->control = MPDDRC_CR_NR_14_ROW_BITS
+	              | MPDDRC_CR_NC_10_COL_BITS
+	              | MPDDRC_CR_CAS_DDR_CAS3
+	              | MPDDRC_CR_DLL_RESET_DISABLED
+	              | MPDDRC_CR_DQMS_NOT_SHARED
+	              | MPDDRC_CR_ENRDM_OFF
+	              | MPDDRC_CR_NB_8_BANKS
+	              | MPDDRC_CR_NDQS_DISABLED
+	              | MPDDRC_CR_UNAL_SUPPORTED
+	              | MPDDRC_CR_OCD_DDR2_EXITCALIB;
 
-	desc->control = MPDDRC_CR_NR_14_ROW_BITS |
-		MPDDRC_CR_NC_10_COL_BITS |
-		MPDDRC_CR_CAS_DDR_CAS3 |
-		MPDDRC_CR_DLL_RESET_DISABLED |
-		MPDDRC_CR_DQMS_NOT_SHARED |
-		MPDDRC_CR_ENRDM_OFF |
-		MPDDRC_CR_NB_8_BANKS |
-		MPDDRC_CR_NDQS_DISABLED |
-		MPDDRC_CR_UNAL_SUPPORTED | MPDDRC_CR_OCD_DDR2_EXITCALIB;
+	desc->io_calibr = MPDDRC_IO_CALIBR_CALCODEP(7)
+	                | MPDDRC_IO_CALIBR_CALCODEN(8)
+	                | MPDDRC_IO_CALIBR_RDIV_RZQ_60
+	                | MPDDRC_IO_CALIBR_TZQIO(5)
+	                | MPDDRC_IO_CALIBR_EN_CALIB_ENABLE_CALIBRATION;
 
 	/* timings */
-	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(45, mck))    // 45ns
-		| MPDDRC_TPR0_TRCD(NS2CYCLES(15, mck))           // 15ns
-		| MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))            // 15ns
-		| MPDDRC_TPR0_TRC(NS2CYCLES(55, mck))            // 55ns
-		| MPDDRC_TPR0_TRP(NS2CYCLES(15, mck))            // 15ns
-		| MPDDRC_TPR0_TRRD(NS2CYCLES(13, mck))           // 12.5ns
-		| MPDDRC_TPR0_TWTR(NS2CYCLES(10, mck))           // 10ns
-		| MPDDRC_TPR0_TMRD(NS2CYCLES(8, mck));           // 8ns
 
-	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(198, mck))   // 198ns
-		| MPDDRC_TPR1_TXSNR(NS2CYCLES(208, mck))         // (tRFC)+10 = 208ns
-		| MPDDRC_TPR1_TXSRD(200)                         // 200ck
-		| MPDDRC_TPR1_TXP(2);                            // 2ck
+	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(45, mck)) // 45ns
+	           | MPDDRC_TPR0_TRCD(NS2CYCLES(15, mck)) // 15ns
+	           | MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR0_TRC(NS2CYCLES(55, mck))  // 55ns
+	           | MPDDRC_TPR0_TRP(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR0_TRRD(NS2CYCLES(13, mck)) // 12.5ns
+	           | MPDDRC_TPR0_TWTR(NS2CYCLES(10, mck)) // 10ns
+	           | MPDDRC_TPR0_TMRD(NS2CYCLES(8, mck)); // 8ns
 
-	desc->tpr2 = MPDDRC_TPR2_TXARD(8)                    // 8ck
-		| MPDDRC_TPR2_TXARDS(2)                          // 2ck
-		| MPDDRC_TPR2_TRPA(NS2CYCLES(15, mck))           // 15ns
-		| MPDDRC_TPR2_TRTP(NS2CYCLES(8, mck))            // 8ns
-		| MPDDRC_TPR2_TFAW(NS2CYCLES(45, mck));          // 45ns
+	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(198, mck))  // 198ns
+	           | MPDDRC_TPR1_TXSNR(NS2CYCLES(208, mck)) // (tRFC)+10 = 208ns
+	           | MPDDRC_TPR1_TXSRD(200)                 // 200ck
+	           | MPDDRC_TPR1_TXP(2);                    // 2ck
+
+	desc->tpr2 = MPDDRC_TPR2_TXARD(8)                  // 8ck
+	           | MPDDRC_TPR2_TXARDS(2)                 // 2ck
+	           | MPDDRC_TPR2_TRPA(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR2_TRTP(NS2CYCLES(8, mck))   // 8ns
+	           | MPDDRC_TPR2_TFAW(NS2CYCLES(45, mck)); // 45ns
 
 	desc->bank = 8192;
 }
@@ -256,41 +266,45 @@ static void _init_mt42l128m16(struct _mpddrc_desc* desc)
 	uint32_t mck = pmc_get_master_clock() / 1000000;
 
 	desc->type = MPDDRC_TYPE_LPDDR2;
+
 	/* DBW = 0 (32 bits bus wide);
 	 * Memory Device = 7 = LPDDR2-SDRAM = 0x00000007 */
-	desc->mode = MPDDRC_MD_MD_LPDDR2_SDRAM | MPDDRC_MD_DBW_DBW_32_BITS;
+	desc->mode = MPDDRC_MD_MD_LPDDR2_SDRAM
+	           | MPDDRC_MD_DBW_DBW_32_BITS;
 
 	desc->data_path = MPDDRC_RD_DATA_PATH_SHIFT_SAMPLING_SHIFT_ONE_CYCLE;
 
+	desc->control = MPDDRC_CR_NR_14_ROW_BITS
+	              | MPDDRC_CR_NC_10_COL_BITS
+	              | MPDDRC_CR_CAS_DDR_CAS3
+	              | MPDDRC_CR_ENRDM_OFF
+	              | MPDDRC_CR_NB_8_BANKS
+	              | MPDDRC_CR_UNAL_SUPPORTED;
+
 	desc->io_calibr = MPDDRC_IO_CALIBR_RDIV_RZQ_60;
 
-	desc->control = MPDDRC_CR_NR_14_ROW_BITS |
-		MPDDRC_CR_NC_10_COL_BITS |
-		MPDDRC_CR_CAS_DDR_CAS3 |
-		MPDDRC_CR_ENRDM_OFF |
-		MPDDRC_CR_NB_8_BANKS |
-		MPDDRC_CR_UNAL_SUPPORTED;
-
 	/* timings */
-	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(40, mck))  // 40ns
-		| MPDDRC_TPR0_TRCD(NS2CYCLES(15, mck))         // 15ns
-		| MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))          // 15ns
-		| MPDDRC_TPR0_TRC(NS2CYCLES(60, mck))          // 60ns
-		| MPDDRC_TPR0_TRP(NS2CYCLES(15, mck))          // 15ns
-		| MPDDRC_TPR0_TRRD(NS2CYCLES(11, mck))         // 11ns
-		| MPDDRC_TPR0_TWTR(NS2CYCLES(8, mck))          // 7.5ns
-		| MPDDRC_TPR0_TMRD(2);                         // 2 clock cycles
 
-	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(130, mck)) // 130ns
-		| MPDDRC_TPR1_TXSNR(NS2CYCLES(140, mck))       // 140ns
-		| MPDDRC_TPR1_TXSRD(200)                       // 200ck
-		| MPDDRC_TPR1_TXP(2);                          // 2ck
+	desc->tpr0 = MPDDRC_TPR0_TRAS(NS2CYCLES(40, mck)) // 40ns
+	           | MPDDRC_TPR0_TRCD(NS2CYCLES(15, mck)) // 15ns
+	           | MPDDRC_TPR0_TWR(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR0_TRC(NS2CYCLES(60, mck))  // 60ns
+	           | MPDDRC_TPR0_TRP(NS2CYCLES(15, mck))  // 15ns
+	           | MPDDRC_TPR0_TRRD(NS2CYCLES(11, mck)) // 11ns
+	           | MPDDRC_TPR0_TWTR(NS2CYCLES(8, mck))  // 7.5ns
+	           | MPDDRC_TPR0_TMRD(2);                 // 2 clock cycles
+
+	desc->tpr1 = MPDDRC_TPR1_TRFC(NS2CYCLES(130, mck))  // 130ns
+	           | MPDDRC_TPR1_TXSNR(NS2CYCLES(140, mck)) // 140ns
+	           | MPDDRC_TPR1_TXSRD(200)                 // 200ck
+	           | MPDDRC_TPR1_TXP(2);                    // 2ck
 
 	desc->tpr2 = MPDDRC_TPR2_TXARD(2)                  // 2ck
-		| MPDDRC_TPR2_TXARDS(2)                        // 2ck
-		| MPDDRC_TPR2_TRPA(NS2CYCLES(18, mck))         // 18ns
-		| MPDDRC_TPR2_TRTP(NS2CYCLES(8, mck))          // 8ns
-		| MPDDRC_TPR2_TFAW(NS2CYCLES(50, mck));        // 50ns
+	           | MPDDRC_TPR2_TXARDS(2)                 // 2ck
+	           | MPDDRC_TPR2_TRPA(NS2CYCLES(18, mck))  // 18ns
+	           | MPDDRC_TPR2_TRTP(NS2CYCLES(8, mck))   // 8ns
+	           | MPDDRC_TPR2_TFAW(NS2CYCLES(50, mck)); // 50ns
+
 	desc->bank = 8192;
 }
 
