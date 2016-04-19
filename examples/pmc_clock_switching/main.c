4bf9013c054bf4bacf07d75db7cf291bd851ea1f
Laurent Fazio <laurent.fazio@atmel.com>
2016-04-19 16:25:23 +0200

    examples/pmc_clock_switching: modularize per chip the pmc configuration
    Signed-off-by: Laurent Fazio <laurent.fazio@atmel.com>

--- examples/pmc_clock_switching/main.c
+++ examples/pmc_clock_switching/main.c
@@ -107,6 +107,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#include "clk-config.h"
+
 #include "trace.h"
 #include "compiler.h"
 
@@ -114,61 +116,6 @@
  *        Local definitions
  *----------------------------------------------------------------------------
  */
-
-struct pck_mck_cfg clock_test_setting[4] = {
-	/* PLLA = 996, PCK = 498, MCK = 166 MHz */
-	/* PLLA EXT12M EXT32K MULA=83 DIV2ON=1 PRES=0 MDIV=3 */
-	{
-		.pck_input = PMC_MCKR_CSS_PLLA_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 82,
-		.plla_div = 1,
-		.plla_div2 = true,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_PCK_DIV3,
-		.h32mxdiv2 = true,
-	},
-	/* PCK = UPLL, MCK = UPLL/3 */
-	/* UPLL EXT12M EXT32K MULA=0 DIV2ON=0 PRES=0 MDIV=3 */
-	{
-		.pck_input = PMC_MCKR_CSS_UPLL_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_PCK_DIV3,
-		.h32mxdiv2 = true,
-	},
-	/* PCK = MCK = 12MHz */
-	/* MAIN EXT12M EXT32K MULA=0 DIV2ON=0 PRES=0 MDIV=0 */
-	{
-		.pck_input = PMC_MCKR_CSS_MAIN_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-	/* PCK = MCK = 32 kHz */
-	/* slow clock EXT12M EXT32K MULA=0 DIV2ON=0 PRES=0 MDIV=0 */
-	{
-		.pck_input = PMC_MCKR_CSS_SLOW_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-};
 
 #define MENU_STRING_LENGTH   200
 #define MENU_NB_OPTIONS      8
