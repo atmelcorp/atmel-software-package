5438ad28efcd23ada6795db9a2bf568e4d98e959
Laurent Fazio <laurent.fazio@atmel.com>
2016-04-21 14:56:26 +0200

    examples/low_power_mode: Modularize clock configuration per chip family
    In order to make easy to speocialize examples to specific target, it is
    important to extract the clock configuration from the main to differentiate
    what is specific to chip. Prepare the inclusion of SAMA5D3 chip family.
    
    Signed-off-by: Laurent Fazio <laurent.fazio@atmel.com>

--- examples/low_power_mode/main.c
+++ examples/low_power_mode/main.c
@@ -134,6 +134,8 @@
 #include "peripherals/tc.h"
 #include "peripherals/pit.h"
 
+#include "clk-config.h"
+
 #include "trace.h"
 #include "compiler.h"
 
@@ -141,113 +143,6 @@
  *        Local definitions
  *----------------------------------------------------------------------------
  */
-
-struct pck_mck_cfg clock_test_setting[] = {
-	/* PLL A = 996, PCK = 498, MCK = 166 MHz */
-	/* PLLA EXT12M EXT32K MULA=82 DIV=1 DIV2ON=1 PRES=0 MDIV=3 h32mxdiv2=1*/
-	{
-		.pck_input = PMC_MCKR_CSS_PLLA_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 82,
-		.plla_div = 1,
-		.plla_div2 = true,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_PCK_DIV3,
-		.h32mxdiv2 = true,
-	},
-	/* PCK = MCK = 12MHz */
-	/* MAIN EXT12M EXT32K MULA=0 DIV=1 DIV2ON=0 PRES=0 MDIV=0 h32mxdiv2=0*/
-	{
-		.pck_input = PMC_MCKR_CSS_MAIN_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-	/* PCK = MCK = 750 kHz */
-	/* MAIN EXT12M EXT32K MULA=0 DIV=1 DIV2ON=0 PRES=16 MDIV=0 h32mxdiv2=0*/
-	{
-		.pck_input = PMC_MCKR_CSS_MAIN_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK_DIV16,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-	/* PCK = MCK = 187.5 kHz */
-	/* MAIN EXT12M EXT32K MULA=0 DIV=1 DIV2ON=0 PRES=64 MDIV=0 h32mxdiv2=0*/
-	{
-		.pck_input = PMC_MCKR_CSS_MAIN_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK_DIV64,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-	/* PCK = MCK = 32 kHz */
-	/* slow clock EXT12M EXT32K MULA=0 DIV=1 DIV2ON=0 PRES=0 MDIV=0 h32mxdiv2=0*/
-	{
-		.pck_input = PMC_MCKR_CSS_SLOW_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-	/* PCK = MCK = 512 Hz */
-	/* slow clock EXT12M EXT32K MULA=0 DIV=1 DIV2ON=0 PRES=64 MDIV=0 h32mxdiv2=0*/
-	{
-		.pck_input = PMC_MCKR_CSS_SLOW_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK_DIV64,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-        /* PCK = MCK = RC12MHz */
-	/* MAIN RC12M RC32K MULA=0 DIV=1 DIV2ON=0 PRES=0 MDIV=0 h32mxdiv2=0*/
-	{
-		.pck_input = PMC_MCKR_CSS_MAIN_CLK,
-		.ext12m = false,
-		.ext32k = false,
-		.plla_mul = 0,
-		.plla_div = 1,
-		.plla_div2 = false,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_EQ_PCK,
-		.h32mxdiv2 = false,
-	},
-	/* PLL A = 792, PCK = 396, MCK = 132MHz */
-	/* MAIN EXT12M EXT32K MULA=66 DIV=1 DIV2ON=1 PRES=0 MDIV=3 h32mxdiv2=1*/
-	{
-		.pck_input = PMC_MCKR_CSS_PLLA_CLK,
-		.ext12m = true,
-		.ext32k = true,
-		.plla_mul = 65,
-		.plla_div = 1,
-		.plla_div2 = true,
-		.pck_pres = PMC_MCKR_PRES_CLOCK,
-		.mck_div = PMC_MCKR_MDIV_PCK_DIV3,
-		.h32mxdiv2 = true,
-	},
-};
 
 #define MENU_NB_OPTIONS 9
 #define MENU_STRING_LENGTH 200
