b6d1382fe56428d4025a9269be231978391d3d96
LI BIN <bin.li@atmel.com>
2016-04-29 14:18:54 +0800

    examples/crypto_qspi_aesb: New QSPI AESB example.
    Signed-off-by: LI BIN <bin.li@atmel.com>

---
+++ examples/crypto_qspi_aesb/TESTING.md
@@ -0,0 +1,51 @@
+AESB EXAMPLE
+============
+
+# Objectives
+------------
+This example aims to protect electronic data with Advanced Encryption Standard
+Bridge (AESB).
+
+# Example Description
+---------------------
+This example shows how to configure AESB to protect electronic data. The
+Automatic Bridge mode, when the AESB block is connected between the system bus
+and QSPI, provides automatic encryption/decryption to/from QSPI without 
+any action on the part of the user.
+
+# Test
+------
+
+## Setup
+--------
+On the computer, open and configure a terminal application
+(e.g. HyperTerminal on Microsoft Windows) with these settings:
+ - 115200 bauds
+ - 8 bits of data
+ - No parity
+ - 1 stop bit
+ - No flow control
+
+## Start the application (SAMA5D2-XPLAINED)
+--------
+
+Tested with IAR and GCC (sram and ddram configuration)
+
+In order to test this example, the process is the following:
+
+Step | Description | Expected Result | Result
+-----|-------------|-----------------|-------
+Transfer data to address of AESB IP scope | the data is ciphered on-the-fly | PASSED | PASSED
+Read data from address of AESB IP scope | the data is deciphered on-the-fly | PASSED | PASSED
+Read data from address outside of AESB IP scope | the data can not be deciphered | FAILED | PASSED
+
+
+# Log
+------
+
+## Current version
+--------
+ - v1.4
+
+## History
+--------
---
+++ examples/crypto_qspi_aesb/main.c
@@ -0,0 +1,270 @@
+/* ----------------------------------------------------------------------------
+ *         SAM Software Package License
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2016, Atmel Corporation
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the disclaimer below.
+ *
+ * Atmel's name may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * DISCLAIMER: THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * \page crypto_qspi_aesb Crypto QSPI AESB Example
+ *
+ * \section Purpose
+ *
+ * This example aims to protect electronic data with Advanced Encryption Standard
+ * Bridge (AESB).
+ *
+ * \section Requirements
+ *
+ * This package can be used on SAMA5D2x Xplained board.
+ *
+ * \section Description
+ *
+ * This example shows how to configure AESB to protect electronic data. The
+ * Automatic Bridge mode, when the AESB block is connected between the system bus
+ * and QSPI, provides automatic encryption/decryption to/from QSPI without 
+ * any action on the part of the user.
+ *
+ * \section Usage
+ *
+ *  -# Build the program and download it inside the evaluation board. Please
+ *     refer to the
+ *     <a href="http://www.atmel.com/dyn/resources/prod_documents/6421B.pdf">
+ *     SAM-BA User Guide</a>, the
+ *     <a href="http://www.atmel.com/dyn/resources/prod_documents/doc6310.pdf">
+ *     GNU-Based Software Development</a>
+ *     application note or to the
+ *     <a href="ftp://ftp.iar.se/WWWfiles/arm/Guides/EWARM_UserGuide.ENU.pdf">
+ *     IAR EWARM User Guide</a>,
+ *     depending on your chosen solution.
+ *  -# On the computer, open and configure a terminal application (e.g.
+ *     HyperTerminal on Microsoft Windows) with these settings:
+ *        - 115200 bauds
+ *        - 8 data bits
+ *        - No parity
+ *        - 1 stop bit
+ *        - No flow control
+ *  -# Start the application. The following traces shall appear on the terminal:
+ *     \code
+ *      -- Crypto QSPI AESB Example xxx --
+ *      -- SAMxxxxx-xx
+ *      -- Compiled: xxx xx xxxx xx:xx:xx --
+ *      QSPI drivers initialized
+ *    \endcode
+ * \section References
+ * - crypto_qspi_aesb/main.c
+ * - qspiflash.c
+ */
+
+/**
+ * \file
+ *
+ * This file contains all the specific code for the qspi_xip example.
+ */
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "board.h"
+#include "chip.h"
+#include "trace.h"
+#include "compiler.h"
+
+#include "peripherals/aic.h"
+#include "peripherals/pio.h"
+#include "peripherals/pmc.h"
+#include "peripherals/qspi.h"
+#include "peripherals/wdt.h"
+#include "peripherals/aesb.h"
+
+#include "memories/qspiflash.h"
+#include "misc/console.h"
+
+#include "power/act8945a.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+/*----------------------------------------------------------------------------
+ *        Local variables
+ *----------------------------------------------------------------------------*/
+
+#ifdef CONFIG_HAVE_PMIC_ACT8945A
+struct _pin act8945a_pins[] = ACT8945A_PINS;
+struct _twi_desc act8945a_twid = {
+	.addr = ACT8945A_ADDR,
+	.freq = ACT8945A_FREQ,
+	.transfert_mode = TWID_MODE_POLLING
+};
+struct _act8945a act8945a = {
+	.desc = {
+		.pin_chglev = ACT8945A_PIN_CHGLEV,
+		.pin_irq = ACT8945A_PIN_IRQ,
+		.pin_lbo = ACT8945A_PIN_LBO
+	}
+};
+#endif
+
+/** Pins to configure for the application. */
+static struct _pin pins_qspi[] = QSPIFLASH_PINS;
+
+/** QSPI serial flash instance. */
+static struct _qspiflash flash;
+
+/*----------------------------------------------------------------------------
+ *        Global functions
+ *----------------------------------------------------------------------------*/
+
+/**
+ *  \brief CRYPRO_QSPI_AESB Application entry point.
+ *
+ *  \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+	bool verify_failed = 0;
+	uint32_t baudrate, idx;
+	uint8_t buffer[4096];
+	uint8_t buffer_read[4096];
+
+	/* Disable watchdog */
+	wdt_disable();
+
+	/* Disable all PIO interrupts */
+	pio_reset_all_it();
+
+	/* Configure console */
+	board_cfg_console();
+	console_clear_screen();
+	console_reset_cursor();
+
+	printf("-- CRPYTO QSPI AESB Example %s --\n\r", SOFTPACK_VERSION);
+	printf("-- %s\n\r", BOARD_NAME);
+	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+
+#ifdef CONFIG_HAVE_PMIC_ACT8945A
+	pio_configure(act8945a_pins, ARRAY_SIZE(act8945a_pins));
+	if (act8945a_configure(&act8945a, &act8945a_twid)) {
+		act8945a_set_regulator_voltage(&act8945a, 6, 2500);
+		act8945a_enable_regulator(&act8945a, 6, true);
+	} else {
+		printf("-E- Error initializing ACT8945A PMIC\n\r");
+	}
+#endif
+
+	/* Enable peripheral clock */
+	pmc_enable_peripheral(ID_AESB);
+	/* Perform a software-triggered hardware reset of the AES interface */
+	aesb_swrst();
+
+	printf("-I- Configure AESB in automatic bridge mode: AES CTR selected\n\r");
+	/* Enable AESB automatic bridge mode */
+	aesb_configure( AESB_MR_AAHB
+					| AESB_MR_DUALBUFF_ACTIVE
+					| AESB_MR_PROCDLY(0)
+					| AESB_MR_SMOD_AUTO_START
+					| AESB_MR_OPMOD_CTR
+					| AESB_MR_CKEY_PASSWD
+					);
+
+	/* Initialize the QSPI and serial flash */
+	pio_configure(pins_qspi, ARRAY_SIZE(pins_qspi));
+
+	trace_debug("Initializing QSPI drivers...\n\r");
+	qspi_initialize(QSPIFLASH_ADDR);
+	trace_debug("QSPI drivers initialized.\n\r");
+
+	baudrate = qspi_set_baudrate(QSPIFLASH_ADDR, QSPIFLASH_BAUDRATE);
+	trace_debug("QSPI baudrate set to %uHz\r\n", (unsigned)baudrate);
+
+	printf("Configuring QSPI Flash...\n\r");
+	if (!qspiflash_configure(&flash, QSPIFLASH_ADDR)) {
+		trace_fatal("Configure QSPI Flash failed!\n\r");
+	}
+	printf("QSPI Flash configured.\n\r");
+
+	printf("-I- Enable QSPI AESB IP scope (0x900000000-0x980000000)\n\r");
+	qspiflash_use_aesb(&flash, true);
+
+	/* Write 64 word buffer with walking bit pattern (0x01, 0x02, ...) */
+	for (idx = 0; idx < ARRAY_SIZE(buffer); idx++){
+		buffer[idx] = 1 << (idx % 8);
+	}
+
+	if (!qspiflash_erase_block(&flash, 0, 4096)) {
+		trace_fatal("QSPI Flash block erase failed!\n\r");
+	}
+
+	printf("-I- Writing to address of QSPI AESB IP scope, the data is encrypted automatically\n\r");
+	if (!qspiflash_write(&flash, 0, buffer, ARRAY_SIZE(buffer))) {
+		trace_fatal("QSPI Flash writing failed!\n\r");
+	}
+
+	printf("-I- Read from address of QSPI AESB IP scope\n\r");
+	memset(buffer_read, 0, ARRAY_SIZE(buffer_read));
+	if (!qspiflash_read(&flash, 0, buffer_read, ARRAY_SIZE(buffer_read))) {
+			trace_fatal("Read the code from QSPI Flash failed!\n\r");
+	}
+	verify_failed = false;
+	printf("-I- Read and verify data from address of AESB IP scope\r\n");
+	for (idx = 0; idx < ARRAY_SIZE(buffer_read); idx++) {
+		if (buffer_read[idx] != buffer[idx]) {
+			verify_failed = true;
+			printf("-E- Data does not match at 0x%x (0x%02x != 0x%02x)\n\r",
+				   (unsigned)(buffer_read + idx), buffer_read[idx], buffer[idx]);
+			break;
+		}
+	}
+	if (!verify_failed) {
+		printf("\r\n-I- As expected, it automatically decrypts the data read from the target slave before putting it on the system bus\r\n");
+	}
+	
+	printf("\r\n-I- Read data from address outside of AESB IP scope. This test is expeted to fail.\r\n");
+	
+	qspiflash_use_aesb(&flash, false);
+
+	printf("-I- Read buffer without using AESB IP scope\n\r");
+	memset(buffer_read, 0, ARRAY_SIZE(buffer_read));
+	if (!qspiflash_read(&flash, 0, buffer_read, ARRAY_SIZE(buffer_read))) {
+			trace_fatal("Read the code from QSPI Flash failed!\n\r");
+	}
+	
+	verify_failed = false;
+	printf("-I- Read and verify data from address 0xD00000000 \r\n");
+	for (idx = 0; idx < ARRAY_SIZE(buffer_read); idx++) {
+		if (buffer_read[idx] != buffer[idx]) {
+			verify_failed = true;
+			printf("-E- Data does not match at 0x%x (0x%02x != 0x%02x)\n\r",
+				   (unsigned)(buffer_read + idx), buffer_read[idx], buffer[idx]);
+			break;
+		}
+	}
+	if (verify_failed) {
+		printf("\r\n-I- As expected, data cannot be decrypted from address outside of AESB IP scope\r\n");
+	}
+	while (1);
+}
+
