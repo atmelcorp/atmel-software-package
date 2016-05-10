87edfd45159f867ea6af3b560374209cb7c2a847
Loic Lefort <loic.lefort@atmel.com>
2016-05-10 17:48:01 +0200

    samba_applets/lowlevel: setup clocks without calling low_level_init
    low_level_init now does too much initialization for applets: we only need clock
    setup.
    
    Signed-off-by: Loic Lefort <loic.lefort@atmel.com>

--- samba_applets/lowlevel/main.c
+++ samba_applets/lowlevel/main.c
@@ -36,43 +36,62 @@
 #include "board.h"
 #include "board_lowlevel.h"
 #include "trace.h"
-
-/*----------------------------------------------------------------------------
- *        Local definitions
- *----------------------------------------------------------------------------*/
-
-/* Initialization mode */
-
-#define INIT_MODE_EK           0
-#define INIT_MODE_USER_CRYSTAL 1
-#define INIT_MODE_BYPASS       2
+#include "peripherals/pmc.h"
 
 /*----------------------------------------------------------------------------
  *         Private functions
  *----------------------------------------------------------------------------*/
 
+static void applet_low_level_init(void)
+{
+#if defined(CONFIG_SOC_SAMA5D2)
+	pmc_switch_mck_to_slck();
+	pmc_set_mck_h32mxdiv(PMC_MCKR_H32MXDIV_H32MXDIV2);
+	pmc_set_mck_plla_div(PMC_MCKR_PLLADIV2);
+	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
+	pmc_set_mck_divider(PMC_MCKR_MDIV_EQ_PCK);
+	pmc_set_plla(0, 0);
+	pmc_select_external_osc();
+	pmc_set_plla(CKGR_PLLAR_ONE | CKGR_PLLAR_PLLACOUNT(0x3F) |
+		CKGR_PLLAR_OUTA(0x0) | CKGR_PLLAR_MULA(82) |
+		CKGR_PLLAR_DIVA_BYPASS, 0);
+	pmc_set_mck_divider(PMC_MCKR_MDIV_PCK_DIV3);
+	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
+	pmc_switch_mck_to_pll();
+#elif defined(CONFIG_SOC_SAMA5D4)
+	pmc_select_external_osc();
+	pmc_switch_mck_to_main();
+	pmc_set_mck_plla_div(PMC_MCKR_PLLADIV2);
+	pmc_set_plla(CKGR_PLLAR_ONE | CKGR_PLLAR_PLLACOUNT(0x3F) |
+		     CKGR_PLLAR_OUTA(0x0) | CKGR_PLLAR_MULA(87) |
+		     CKGR_PLLAR_DIVA_BYPASS, PMC_PLLICPR_IPLL_PLLA(0x0));
+	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
+	pmc_set_mck_divider(PMC_MCKR_MDIV_PCK_DIV3);
+	pmc_switch_mck_to_pll();
+#endif
+}
+
 static uint32_t handle_cmd_initialize(uint32_t cmd, uint32_t *mailbox)
 {
 	union initialize_mailbox *mbx = (union initialize_mailbox*)mailbox;
-	uint32_t mode = mbx->in.parameters[0];
 
 	assert(cmd == APPLET_CMD_INITIALIZE);
 
 	applet_set_init_params(mbx->in.comm_type, mbx->in.trace_level);
 
-	switch (mode) {
-	case INIT_MODE_EK:
-		low_level_init();
-		trace_info_wp("\r\nApplet 'Low-Level' from softpack " SOFTPACK_VERSION ".\r\n");
-		trace_info_wp("Low-Level initialization complete.\r\n");
-		return APPLET_SUCCESS;
+	trace_info_wp("\r\nApplet 'Low-Level' from softpack " SOFTPACK_VERSION ".\r\n");
 
-	case INIT_MODE_USER_CRYSTAL:
-	case INIT_MODE_BYPASS:
-	default:
-		// unknown mode or not implemented
-		return APPLET_FAIL;
-	}
+	trace_info_wp("Current processor clock: %dHz\r\n",
+			(unsigned)pmc_get_processor_clock());
+	applet_low_level_init();
+
+	/* re-inititialize console after clock setup */
+	applet_set_init_params(mbx->in.comm_type, mbx->in.trace_level);
+
+	trace_info_wp("Low-Level initialization complete.\r\n");
+	trace_info_wp("Processor clock: %dHz\r\n",
+			(unsigned)pmc_get_processor_clock());
+	return APPLET_SUCCESS;
 }
 
 /*----------------------------------------------------------------------------
