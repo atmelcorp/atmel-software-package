010deafff83adb92cb1b0703314e10192e56a6e9
Loic Lefort <loic.lefort@atmel.com>
2016-06-15 14:22:04 +0200

    samba_applets/lowlevel: use board_cfg_clocks function from board_support
    Signed-off-by: Loic Lefort <loic.lefort@atmel.com>

--- samba_applets/lowlevel/main.c
+++ samba_applets/lowlevel/main.c
@@ -34,54 +34,13 @@
 #include <assert.h>
 #include "applet.h"
 #include "board.h"
-#include "board_lowlevel.h"
+#include "board_support.h"
 #include "trace.h"
 #include "peripherals/pmc.h"
 
 /*----------------------------------------------------------------------------
  *         Private functions
  *----------------------------------------------------------------------------*/
-
-static void applet_low_level_init(void)
-{
-#if defined(CONFIG_SOC_SAMA5D2)
-	pmc_switch_mck_to_slck();
-	pmc_set_mck_h32mxdiv(PMC_MCKR_H32MXDIV_H32MXDIV2);
-	pmc_set_mck_plla_div(PMC_MCKR_PLLADIV2);
-	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
-	pmc_set_mck_divider(PMC_MCKR_MDIV_EQ_PCK);
-	pmc_set_plla(0, 0);
-	pmc_select_external_osc();
-	pmc_set_plla(CKGR_PLLAR_ONE | CKGR_PLLAR_PLLACOUNT(0x3F) |
-		CKGR_PLLAR_OUTA(0x0) | CKGR_PLLAR_MULA(82) |
-		CKGR_PLLAR_DIVA_BYPASS, 0);
-	pmc_set_mck_divider(PMC_MCKR_MDIV_PCK_DIV3);
-	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
-	pmc_switch_mck_to_pll();
-#elif defined(CONFIG_SOC_SAMA5D3)
-	pmc_select_external_osc();
-	pmc_switch_mck_to_main();
-	pmc_set_mck_plla_div(PMC_MCKR_PLLADIV2);
-	pmc_set_plla(CKGR_PLLAR_ONE | CKGR_PLLAR_PLLACOUNT(0x3F) |
-		     CKGR_PLLAR_OUTA(0x0) | CKGR_PLLAR_MULA(87) |
-		     CKGR_PLLAR_DIVA_BYPASS, PMC_PLLICPR_IPLL_PLLA(3));
-	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
-	pmc_set_mck_divider(PMC_MCKR_MDIV_PCK_DIV3);
-	pmc_switch_mck_to_pll();
-#elif defined(CONFIG_SOC_SAMA5D4)
-	pmc_select_external_osc();
-	pmc_switch_mck_to_main();
-	pmc_set_mck_plla_div(PMC_MCKR_PLLADIV2);
-	pmc_set_plla(CKGR_PLLAR_ONE | CKGR_PLLAR_PLLACOUNT(0x3F) |
-		     CKGR_PLLAR_OUTA(0x0) | CKGR_PLLAR_MULA(87) |
-		     CKGR_PLLAR_DIVA_BYPASS, PMC_PLLICPR_IPLL_PLLA(0x0));
-	pmc_set_mck_prescaler(PMC_MCKR_PRES_CLOCK);
-	pmc_set_mck_divider(PMC_MCKR_MDIV_PCK_DIV3);
-	pmc_switch_mck_to_pll();
-#else
-#error Unsupported SoC!
-#endif
-}
 
 static uint32_t handle_cmd_initialize(uint32_t cmd, uint32_t *mailbox)
 {
@@ -95,7 +54,7 @@ static uint32_t handle_cmd_initialize(uint32_t cmd, uint32_t *mailbox)
 
 	trace_info_wp("Current processor clock: %dHz\r\n",
 			(unsigned)pmc_get_processor_clock());
-	applet_low_level_init();
+	board_cfg_clocks();
 
 	/* re-inititialize console after clock setup */
 	applet_set_init_params(mbx->in.comm_type, mbx->in.trace_level);
