43d239bc82072a35b5e8c36759c16788d02aafbd
Tony Han <tony.han@atmel.com>
2016-07-11 13:59:50 +0200

    examples/spi_slave: Fix pins for sama5d4-ek
    Signed-off-by: Tony Han <tony.han@atmel.com>

--- examples/spi_slave/config_sama5d4-ek.h
+++ examples/spi_slave/config_sama5d4-ek.h
@@ -6,7 +6,7 @@
 /** define the CS signal of SPI master */
 #define SPI_MASTER_CS 2
 /** define the pins of SPI master */
-#define SPI_MASTER_PINS PINS_SPI1_NPCS2
+#define SPI_MASTER_PINS PINS_SPI1_NPCS2_ALT
 
 /** define the address of SPI slave */
 #define SPI_SLAVE_ADDR SPI2
