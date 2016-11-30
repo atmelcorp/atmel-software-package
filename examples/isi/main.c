d15058224b184c008dfc80daab841b5cdac4de6b
LI BIN <bin.li@atmel.com>
2016-11-30 13:41:00 +0800

    examples/isi: rework ISI example with to use isid driver
    Signed-off-by: LI BIN <bin.li@atmel.com>

--- examples/isi/main.c
+++ examples/isi/main.c
@@ -88,18 +88,14 @@
 #include "chip.h"
 #include "compiler.h"
 #include "trace.h"
-#include "timer.h"
 
-#include "gpio/pio.h"
-
-#include "misc/cache.h"
 #include "misc/console.h"
 
 #include "peripherals/lcdc.h"
-#include "peripherals/pmc.h"
 
 #include "video/image_sensor_inf.h"
 #include "video/isi.h"
+#include "video/isid.h"
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -109,10 +105,10 @@
  *----------------------------------------------------------------------------*/
 
 /** Frame buffer base address in preview path */
-#define ISI_PREVIEW_PATH_BASE_ADDRESS (DDR_CS_ADDR + 16* 1024 * 1024)
+#define ISI_PREVIEW_PATH_BASE_ADDRESS (DDR_CS_ADDR + 16 * 1024 * 1024)
 
 /** Maximum number of preview buffer */
-#define ISI_MAX_NUM_PREVIEW_BUFFER 2
+#define ISI_MAX_NUM_PREVIEW_BUFFER      1
 
 /** Maximum image size (YUV)*/
 #define ISI_MAX_IMAGE_SIZE (1600 * 1200 * 2)
@@ -121,16 +117,11 @@
 #define LCD_PREVIEW_BASE_ADDRESS ISI_PREVIEW_PATH_BASE_ADDRESS
 
 /** Frame buffer base address in codec path */
-#define ISI_CODEC_PATH_BASE_ADDRESS (ISI_PREVIEW_PATH_BASE_ADDRESS + ISI_MAX_IMAGE_SIZE * ISI_MAX_NUM_PREVIEW_BUFFER)
+#define ISI_CODEC_PATH_BASE_ADDRESS (ISI_PREVIEW_PATH_BASE_ADDRESS +\
+			ISI_MAX_IMAGE_SIZE * ISI_MAX_NUM_PREVIEW_BUFFER)
 
 /** LCD buffer start address for captured image (same address for ISI codec buffer) */
 #define LCD_CAPTURED_BASE_ADDRESS ISI_CODEC_PATH_BASE_ADDRESS
-
-#define ISI_DMA_DESC_PREV_ADDR (ISI_CODEC_PATH_BASE_ADDRESS + ISI_MAX_IMAGE_SIZE)
-#define ISI_DMA_DESC_CODEC_ADDR (ISI_DMA_DESC_PREV_ADDR + 0x1000)
-
-/** TWI clock frequency in Hz. */
-#define TWCK 400000
 
 /** LCDC_OVR1 layer use for preview frame display in RGB output */
 #define LCD_PREVIEW_LAYER LCDC_OVR1
@@ -147,13 +138,9 @@ static struct sensor_profile *sensor;
 
 /* ISI frame buffer descriptors */
 
-CACHE_ALIGNED struct _isi_dma_desc preview_path_desc[ISI_MAX_NUM_PREVIEW_BUFFER + 1];
-
-CACHE_ALIGNED struct _isi_dma_desc codec_path_desc[ISI_MAX_NUM_PREVIEW_BUFFER + 1];
-
 /** LCD buffer.*/
-static uint8_t *pOvr1Buffer = (uint8_t*)LCD_PREVIEW_BASE_ADDRESS;
-static uint8_t *pHeoBuffer =  (uint8_t*)LCD_CAPTURED_BASE_ADDRESS;
+static uint8_t *ovr_buffer = (uint8_t*)LCD_PREVIEW_BASE_ADDRESS;
+static uint8_t *heo_buffer = (uint8_t*)LCD_CAPTURED_BASE_ADDRESS;
 
 /* Image size in preview mode */
 static uint32_t image_width, image_height;
@@ -161,36 +148,13 @@ static uint32_t image_width, image_height;
 /* Image output bit width */
 static uint8_t sensor_bit_width;
 
-static struct _isi_yuv2rgb y2r = { 0x95, 0xFF, 0x68, 0x32, 1, 1, 1, 0xCC };
+static struct _isi_yuv2rgb ref_matrix = { 0x95, 0xFF, 0x68, 0x32, 1, 1, 1, 0xCC };
+
+static struct _isid_desc isid;
 
 /*----------------------------------------------------------------------------
  *        Local functions
  *----------------------------------------------------------------------------*/
-
-/**
- * \brief Set up Frame Buffer Descriptors(FBD) for preview path.
- */
-static void configure_frame_buffer(void)
-{
-	uint32_t i;
-	for(i = 0; i < ISI_MAX_NUM_PREVIEW_BUFFER; i++) {
-		preview_path_desc[i].address = (uint32_t)ISI_PREVIEW_PATH_BASE_ADDRESS ;
-		preview_path_desc[i].control = ISI_DMA_P_CTRL_P_FETCH | ISI_DMA_P_CTRL_P_WB;
-		preview_path_desc[i].next    = (uint32_t)&preview_path_desc[i + 1];
-	}
-	/* Wrapping to first FBD */
-	preview_path_desc[i-1].next = (uint32_t)preview_path_desc;
-
-	for(i = 0; i < ISI_MAX_NUM_PREVIEW_BUFFER; i++) {
-		codec_path_desc[i].address = (uint32_t)ISI_CODEC_PATH_BASE_ADDRESS;
-		codec_path_desc[i].control = ISI_DMA_C_CTRL_C_FETCH | ISI_DMA_C_CTRL_C_WB;
-		codec_path_desc[i].next    = (uint32_t)&codec_path_desc[ i + 1];
-	}
-	/* Wrapping to first FBD */
-	codec_path_desc[i-1].next = (uint32_t)codec_path_desc;
-	cache_clean_region(preview_path_desc, sizeof(preview_path_desc));
-	cache_clean_region(codec_path_desc, sizeof(codec_path_desc));
-}
 
 /**
  * \brief Configure LCD controller.
@@ -200,7 +164,14 @@ static void configure_lcd(void)
 	lcdc_configure_input_mode(LCD_PREVIEW_LAYER, LCDC_BASECFG1_RGBMODE_16BPP_RGB_565);
 	lcdc_configure_input_mode(LCD_CAPTURE_LAYER, LCDC_HEOCFG1_YUVEN |
 							LCDC_HEOCFG1_YUVMODE_16BPP_YCBCR_MODE3);
-	printf("- LCD display on\n\r");
+	lcdc_create_canvas(LCD_PREVIEW_LAYER, ovr_buffer, 16,
+						(BOARD_LCD_WIDTH - image_width) / 2,
+						(BOARD_LCD_HEIGHT - image_height) / 2,
+						image_width, image_height);
+	lcdc_create_canvas(LCD_CAPTURE_LAYER, heo_buffer, 16,
+						(BOARD_LCD_WIDTH - image_width) / 2,
+						(BOARD_LCD_HEIGHT - image_height) / 2,
+						image_width, image_height);
 }
 
 /**
@@ -208,54 +179,22 @@ static void configure_lcd(void)
  */
 static void configure_isi(void)
 {
-	/* Set up Frame Buffer Descriptors(FBD) for preview path. */
-	configure_frame_buffer();
-	/* Reset ISI peripheral */
-	isi_reset();
-	/* Set the windows blank */
-	isi_set_blank(0, 0);
-	/* Set vertical and horizontal Size of the Image Sensor for preview path*/
-	isi_set_sensor_size(image_width, image_height);
-	/* Set preview size to fit LCD size*/
-	isi_set_preview_size(BOARD_LCD_WIDTH, BOARD_LCD_HEIGHT );
-	/* calculate scaler factor automatically. */
-	isi_calc_scaler_factor();
-	/*  Set data stream in YUV/RGB format.*/
-	isi_set_input_stream(0);
-	/* Defines YCrCb swap format.*/
-	isi_ycrcb_format(ISI_CFG2_YCC_SWAP_MODE2);
-	/* Set Matrix color space for YUV to RBG convert */
-	isi_set_matrix_yuv2rgb(&y2r);
+	isid.cfg.input_format = YUV_422;
+	isid.cfg.multi_bufs = ISI_MAX_NUM_PREVIEW_BUFFER;
+	isid.cfg.width = image_width;
+	isid.cfg.height = image_height;
+	isid.cfg.capture_rate = 0;
 
-	/* Configure DMA for preview path. */
-	isi_set_dma_preview_path((uint32_t)preview_path_desc,
-							ISI_DMA_P_CTRL_P_FETCH,
-							(uint32_t)ISI_PREVIEW_PATH_BASE_ADDRESS);
+	isid.pipe.pipe = ISID_PIPE_PREVIEW;
+	isid.pipe.px = BOARD_LCD_WIDTH;
+	isid.pipe.py = BOARD_LCD_HEIGHT;
+	isid.pipe.yuv = ISID_Y_CB_Y_CR;
+	isid.pipe.yuv2rgb_matrix = &ref_matrix;
+	isid.pipe.rgb2yuv_matrix = NULL;
+	isid.dma.address_p = LCD_PREVIEW_BASE_ADDRESS;
+	isid.dma.address_c = LCD_CAPTURED_BASE_ADDRESS;
 
-	/* Configure DMA for preview path. */
-	isi_set_dma_codec_path((uint32_t)codec_path_desc,
-							ISI_DMA_C_CTRL_C_FETCH,
-							(uint32_t)ISI_CODEC_PATH_BASE_ADDRESS);
-
-	lcdc_create_canvas(LCD_PREVIEW_LAYER,
-						pOvr1Buffer,
-						16,
-						(BOARD_LCD_WIDTH -image_width)/2,
-						(BOARD_LCD_HEIGHT -image_height)/2,
-						image_width,
-						image_height);
-	lcdc_create_canvas(LCD_CAPTURE_LAYER,
-						pHeoBuffer,
-						16,
-						(BOARD_LCD_WIDTH -image_width)/2,
-						(BOARD_LCD_HEIGHT -image_height)/2,
-						image_width,
-						image_height);
-	isi_reset();
-	isi_disable_interrupt(0xFFFFFFFF);
-	isi_enable();
-	isi_dma_codec_channel_enabled(1);
-	isi_dma_preview_channel_enabled(1);
+	isid_pipe_start(&isid);
 }
 
 /*----------------------------------------------------------------------------
@@ -274,9 +213,6 @@ extern int main( void )
 	/* Output example information */
 	console_example_info("ISI Example");
 
-	/* Configure LCD */
-	configure_lcd();
-
 	printf("Image sensor detection:\n\r");
 	if ((sensor = sensor_detect(true, 0))) {
 		if (sensor_setup(sensor, VGA, YUV_422) != SENSOR_OK){
@@ -289,11 +225,15 @@ extern int main( void )
 	}
 
 	/* Retrieve sensor output format and size */
-	sensor_get_output(sensor, VGA, YUV_422, &sensor_bit_width, &image_width, &image_height);
+	sensor_get_output(sensor, VGA, YUV_422, &sensor_bit_width,
+					&image_width, &image_height);
 
 	printf("Image attributes : <%x, %u, %u>\n\r", YUV_INPUT,
 			(unsigned)image_width, (unsigned)image_height);
 	printf("preview in RGB 565 mode\n\r");
+
+	/* Configure LCD */
+	configure_lcd();
 
 	/* ISI Initialize */
 	configure_isi();
@@ -306,29 +246,25 @@ extern int main( void )
 		case 'c': case 'C':
 			printf("Capture image from CODEC path in YUV mode\n\r");
 			lcdc_enable_layer(LCD_PREVIEW_LAYER, 0);
-			isi_dma_preview_channel_enabled(0);
-			isi_dma_codec_channel_enabled(1);
+			isid.pipe.pipe = ISID_PIPE_CODEC;
+			isid_pipe_start(&isid);
 			for (delay = 0; delay < 0xffff; delay++);
 			isi_codec_request();
 			isi_codec_wait_dma_completed();
 			printf("-I- Capture done \r\n");
 			for (delay = 0; delay < 0x5ffff; delay++);
-			lcdc_put_image_rotated(LCD_CAPTURE_LAYER,
-								NULL,
-								16,
+			lcdc_put_image_rotated(LCD_CAPTURE_LAYER, NULL, 16,
 								(BOARD_LCD_WIDTH - image_width)/2,
-								(BOARD_LCD_HEIGHT-  image_height)/2,
-								image_width,
-								image_height,
-								image_width,
-								image_height,
+								(BOARD_LCD_HEIGHT- image_height)/2,
+								image_width, image_height,
+								image_width, image_height,
 								0);
 			lcdc_enable_layer(LCD_CAPTURE_LAYER, 1);
 			break;
 		case 'p': case 'P':
 			printf("preivew in RGB 565 mode\n\r");
-			isi_dma_codec_channel_enabled(0);
-			isi_dma_preview_channel_enabled(1);
+			isid.pipe.pipe = ISID_PIPE_PREVIEW;
+			isid_pipe_start(&isid);
 			lcdc_enable_layer(LCD_CAPTURE_LAYER, 0);
 			lcdc_enable_layer(LCD_PREVIEW_LAYER, 1);
 			break;
