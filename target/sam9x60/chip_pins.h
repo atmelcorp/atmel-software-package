atmelcorp/atmel-software-package

commit 0590f6ab2701cfb16ed5a672175edd38a9f54a03

target/sam9x60: add pio definition for flexcom
Signed-off-by: LI BIN <bin.li@microchip.com>

target/sam9x60/chip_pins.h
@@ -535,4 +535,149 @@
 
 /* ============================================================ */
 
+
+/* ========== Pio PIN definition for FLEXCOM0 peripheral ========== */
+/* FLEXCOM0 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM0_IO0_IOS1 { PIO_GROUP_A, PIO_PA0A_FLEXCOM0_IO0, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM0 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM0_IO1_IOS1 { PIO_GROUP_A, PIO_PA1A_FLEXCOM0_IO1, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM0 IO2 (USART: SCK, SPI: SPCK) */
+#define PIN_FLEXCOM0_IO2_IOS1 { PIO_GROUP_A, PIO_PA4A_FLEXCOM0_IO2, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM0 IO3 (USART: CTS, SPI: NPCS0) */
+#define PIN_FLEXCOM0_IO3_IOS1 { PIO_GROUP_A, PIO_PA3A_FLEXCOM0_IO3, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM0 IO4 (USART: RTS, SPI: NPCS1) */
+#define PIN_FLEXCOM0_IO4_IOS1 { PIO_GROUP_A, PIO_PA2A_FLEXCOM0_IO4, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM0 IO7 (USART: LONCOL) */
+#define PIN_FLEXCOM0_IO7_IOS1 { PIO_GROUP_B, PIO_PB6B_FLEXCOM0_IO7, PIO_PERIPH_B, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM1 peripheral ========== */
+/* FLEXCOM1 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM1_IO0_IOS1 { PIO_GROUP_A, PIO_PA5A_FLEXCOM1_IO0, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM1 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM1_IO1_IOS1 { PIO_GROUP_A, PIO_PA6A_FLEXCOM1_IO1, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM1 IO2 (USART: SCK, SPI: SPCK) */
+#define PIN_FLEXCOM1_IO2_IOS1 { PIO_GROUP_C, PIO_PC29C_FLEXCOM1_IO2, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM1 IO3 (USART: CTS, SPI: NPCS0) */
+#define PIN_FLEXCOM1_IO3_IOS1 { PIO_GROUP_C, PIO_PC28C_FLEXCOM1_IO3, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM1 IO4 (USART: RTS, SPI: NPCS1) */
+#define PIN_FLEXCOM1_IO4_IOS1 { PIO_GROUP_C, PIO_PC27C_FLEXCOM1_IO4, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM1 IO7 (USART: LONCOL) */
+#define PIN_FLEXCOM1_IO7_IOS1 { PIO_GROUP_C, PIO_PC17B_FLEXCOM1_IO7, PIO_PERIPH_B, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM2 peripheral ========== */
+/* FLEXCOM2 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM2_IO0_IOS1 { PIO_GROUP_A, PIO_PA7A_FLEXCOM2_IO0, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM2 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM2_IO1_IOS1 { PIO_GROUP_A, PIO_PA8A_FLEXCOM2_IO1, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM2 IO2 (USART: SCK, SPI: SPCK) */
+#define PIN_FLEXCOM2_IO2_IOS1 { PIO_GROUP_B, PIO_PB2B_FLEXCOM2_IO2, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM2 IO3 (USART: CTS, SPI: NPCS0) */
+#define PIN_FLEXCOM2_IO3_IOS1 { PIO_GROUP_B, PIO_PB1B_FLEXCOM2_IO3, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM2 IO4 (USART: RTS, SPI: NPCS1) */
+#define PIN_FLEXCOM2_IO4_IOS1 { PIO_GROUP_B, PIO_PB0B_FLEXCOM2_IO4, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM2 IO7 (USART: LONCOL) */
+#define PIN_FLEXCOM2_IO7_IOS1 { PIO_GROUP_A, PIO_PA29C_FLEXCOM2_IO7, PIO_PERIPH_C, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM3 peripheral ========== */
+/* FLEXCOM3 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM3_IO0_IOS1 { PIO_GROUP_C, PIO_PC22B_FLEXCOM3_IO0, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM3 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM3_IO1_IOS1 { PIO_GROUP_C, PIO_PC23B_FLEXCOM3_IO1, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM3 IO2 (USART: SCK, SPI: SPCK) */
+#define PIN_FLEXCOM3_IO2_IOS1 { PIO_GROUP_C, PIO_PC26B_FLEXCOM3_IO2, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM3 IO3 (USART: CTS, SPI: NPCS0) */
+#define PIN_FLEXCOM3_IO3_IOS1 { PIO_GROUP_C, PIO_PC25B_FLEXCOM3_IO3, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM3 IO4 (USART: RTS, SPI: NPCS1) */
+#define PIN_FLEXCOM3_IO4_IOS1 { PIO_GROUP_C, PIO_PC24B_FLEXCOM3_IO4, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM3 IO7 (USART: LONCOL) */
+#define PIN_FLEXCOM3_IO7_IOS1 { PIO_GROUP_C, PIO_PC30C_FLEXCOM3_IO7, PIO_PERIPH_C, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM4 peripheral ========== */
+/* FLEXCOM4 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM4_IO0_IOS1 { PIO_GROUP_A, PIO_PA12A_FLEXCOM4_IO0, PIO_PERIPH_A, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO0_IOS2 { PIO_GROUP_A, PIO_PA12A_FLEXCOM4_IO0, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM4 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM4_IO1_IOS1 { PIO_GROUP_A, PIO_PA11A_FLEXCOM4_IO1, PIO_PERIPH_A, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO1_IOS2 { PIO_GROUP_A, PIO_PA11A_FLEXCOM4_IO1, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM4 IO2 (USART: SCK, SPI: SPCK) */
+#define PIN_FLEXCOM4_IO2_IOS1 { PIO_GROUP_A, PIO_PA13A_FLEXCOM4_IO2, PIO_PERIPH_A, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO2_IOS2 { PIO_GROUP_A, PIO_PA13A_FLEXCOM4_IO2, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM4 IO3 (USART: CTS, SPI: NPCS0) */
+#define PIN_FLEXCOM4_IO3_IOS1 { PIO_GROUP_A, PIO_PA14A_FLEXCOM4_IO3, PIO_PERIPH_A, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO3_IOS1 { PIO_GROUP_A, PIO_PA14A_FLEXCOM4_IO3, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM4 IO4 (USART: RTS, SPI: NPCS1) */
+#define PIN_FLEXCOM4_IO4_IOS1 { PIO_GROUP_A, PIO_PA7B_FLEXCOM4_IO4, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO4_IOS2 { PIO_GROUP_A, PIO_PA0C_FLEXCOM4_IO4, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM4 IO5 (SPI: NPCS2) */
+#define PIN_FLEXCOM4_IO5_IOS1 { PIO_GROUP_A, PIO_PA1B_FLEXCOM4_IO5, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO5_IOS2 { PIO_GROUP_A, PIO_PA8C_FLEXCOM4_IO5, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM4 IO6 (SPI: NPCS3) */
+#define PIN_FLEXCOM4_IO6_IOS1 { PIO_GROUP_B, PIO_PB3B_FLEXCOM4_IO6, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO6_IOS2 { PIO_GROUP_B, PIO_PB3B_FLEXCOM4_IO6, PIO_PERIPH_B, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM5 peripheral ========== */
+/* FLEXCOM5 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM5_IO0_IOS1 { PIO_GROUP_A, PIO_PA22B_FLEXCOM5_IO0, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO0_IOS2 { PIO_GROUP_A, PIO_PA22B_FLEXCOM5_IO0, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM5 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM5_IO1_IOS1 { PIO_GROUP_A, PIO_PA21B_FLEXCOM5_IO1, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO1_IOS2 { PIO_GROUP_A, PIO_PA21B_FLEXCOM5_IO1, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM5 IO2 (USART: SCK, SPI: SPCK) */
+#define PIN_FLEXCOM5_IO2_IOS1 { PIO_GROUP_A, PIO_PA23B_FLEXCOM5_IO2, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO2_IOS2 { PIO_GROUP_A, PIO_PA23B_FLEXCOM5_IO2, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM5 IO3 (USART: CTS, SPI: NPCS0) */
+#define PIN_FLEXCOM5_IO3_IOS1 { PIO_GROUP_A, PIO_PA8B_FLEXCOM5_IO3, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO3_IOS2 { PIO_GROUP_A, PIO_PA8B_FLEXCOM5_IO3, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM5 IO4 (USART: RTS, SPI: NPCS1) */
+#define PIN_FLEXCOM5_IO4_IOS1 { PIO_GROUP_A, PIO_PA0B_FLEXCOM5_IO4, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO4_IOS2 { PIO_GROUP_A, PIO_PA7C_FLEXCOM5_IO4, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM5 IO5 (SPI: NPCS2) */
+#define PIN_FLEXCOM5_IO5_IOS1 { PIO_GROUP_A, PIO_PA31B_FLEXCOM5_IO5, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO5_IOS2 { PIO_GROUP_A, PIO_PA31B_FLEXCOM5_IO5, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM5 IO6 (SPI: NPCS3) */
+#define PIN_FLEXCOM5_IO6_IOS1 { PIO_GROUP_A, PIO_PA30B_FLEXCOM5_IO6, PIO_PERIPH_B, PIO_DEFAULT }
+#define PIN_FLEXCOM5_IO6_IOS2 { PIO_GROUP_A, PIO_PA30B_FLEXCOM5_IO6, PIO_PERIPH_B, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM6 peripheral ========== */
+/* FLEXCOM6 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM6_IO0_IOS1 { PIO_GROUP_A, PIO_PA30A_FLEXCOM6_IO0, PIO_PERIPH_A, PIO_DEFAULT }
+/* FLEXCOM6 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM6_IO1_IOS1 { PIO_GROUP_A, PIO_PA31A_FLEXCOM6_IO1, PIO_PERIPH_A, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM7 peripheral ========== */
+/* FLEXCOM7 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM7_IO0_IOS1 { PIO_GROUP_C, PIO_PC0C_FLEXCOM7_IO0, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM7 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM7_IO1_IOS1 { PIO_GROUP_C, PIO_PC1C_FLEXCOM7_IO1, PIO_PERIPH_C, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM8 peripheral ========== */
+/* FLEXCOM8 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM8_IO0_IOS1 { PIO_GROUP_B, PIO_PB4B_FLEXCOM8_IO0, PIO_PERIPH_B, PIO_DEFAULT }
+/* FLEXCOM8 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM8_IO1_IOS1 { PIO_GROUP_B, PIO_PB5B_FLEXCOM8_IO1, PIO_PERIPH_B, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM9 peripheral ========== */
+/* FLEXCOM9 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM9_IO0_IOS1 { PIO_GROUP_C, PIO_PC8C_FLEXCOM9_IO0, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM9 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM9_IO1_IOS1 { PIO_GROUP_C, PIO_PC9C_FLEXCOM9_IO1, PIO_PERIPH_C, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM10 peripheral ========== */
+/* FLEXCOM10 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM10_IO0_IOS1 { PIO_GROUP_C, PIO_PC16C_FLEXCOM10_IO0, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM10 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM10_IO1_IOS1 { PIO_GROUP_C, PIO_PC17C_FLEXCOM10_IO1, PIO_PERIPH_C, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM11 peripheral ========== */
+/* FLEXCOM11 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM11_IO0_IOS1 { PIO_GROUP_B, PIO_PB19C_FLEXCOM11_IO0, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM11 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM11_IO1_IOS1 { PIO_GROUP_B, PIO_PB20C_FLEXCOM11_IO1, PIO_PERIPH_C, PIO_DEFAULT }
+
+/* ========== Pio PIN definition for FLEXCOM12 peripheral ========== */
+/* FLEXCOM12 IO0 (USART: TXD, SPI: MOSI, TWI: TWD) */
+#define PIN_FLEXCOM12_IO0_IOS1 { PIO_GROUP_B, PIO_PB21C_FLEXCOM12_IO0, PIO_PERIPH_C, PIO_DEFAULT }
+/* FLEXCOM12 IO1 (USART: RXD, SPI: MISO, TWI: TWCK) */
+#define PIN_FLEXCOM12_IO1_IOS1 { PIO_GROUP_B, PIO_PB22C_FLEXCOM12_IO1, PIO_PERIPH_C, PIO_DEFAULT }
+
 #endif /* _CHIP_PINS_H_ */

commit f205ecc6bd99f14c931d2154e701c32964809245

target/sam9x60: fix typo in FLEXCOM4 ioset2 pin muxing
Fixes: 0590f6ab2701 ("target/sam9x60: add pio definition for flexcom")
Signed-off-by: Cyrille Pitchen <cyrille.pitchen@microchip.com>

target/sam9x60/chip_pins.h
@@ -595,7 +595,7 @@
 #define PIN_FLEXCOM4_IO2_IOS2 { PIO_GROUP_A, PIO_PA13A_FLEXCOM4_IO2, PIO_PERIPH_A, PIO_DEFAULT }
 /* FLEXCOM4 IO3 (USART: CTS, SPI: NPCS0) */
 #define PIN_FLEXCOM4_IO3_IOS1 { PIO_GROUP_A, PIO_PA14A_FLEXCOM4_IO3, PIO_PERIPH_A, PIO_DEFAULT }
-#define PIN_FLEXCOM4_IO3_IOS1 { PIO_GROUP_A, PIO_PA14A_FLEXCOM4_IO3, PIO_PERIPH_A, PIO_DEFAULT }
+#define PIN_FLEXCOM4_IO3_IOS2 { PIO_GROUP_A, PIO_PA14A_FLEXCOM4_IO3, PIO_PERIPH_A, PIO_DEFAULT }
 /* FLEXCOM4 IO4 (USART: RTS, SPI: NPCS1) */
 #define PIN_FLEXCOM4_IO4_IOS1 { PIO_GROUP_A, PIO_PA7B_FLEXCOM4_IO4, PIO_PERIPH_B, PIO_DEFAULT }
 #define PIN_FLEXCOM4_IO4_IOS2 { PIO_GROUP_A, PIO_PA0C_FLEXCOM4_IO4, PIO_PERIPH_C, PIO_DEFAULT }