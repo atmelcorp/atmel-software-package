d5e0918fdc9c0354db2002b35c0a7668b62476ca
Tony Han <tony.han@microchip.com>
2019-07-02 14:48:59 +0800

    drivers/network: fix GMAC data bus width configuration for sama5d2/d4
    Also remove extra "GMAC_NCFGR_DBW_DBW64" definition, introduced in 2e7767c0bd0b5969538295f288daf5b9610481c3
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- drivers/network/gmacd.c
+++ drivers/network/gmacd.c
@@ -48,11 +48,6 @@
  *         Definitions
  *----------------------------------------------------------------------------*/
 
-/* for compatibility with some devices that do not support this flag */
-#ifndef GMAC_NCFGR_DBW_DBW64
-#define GMAC_NCFGR_DBW_DBW64 0
-#endif
-
 // Interrupt bits
 #define GMAC_INT_RX_BITS     (GMAC_IER_RCOMP | GMAC_IER_RXUBR | GMAC_IER_ROVR)
 #define GMAC_INT_TX_ERR_BITS (GMAC_IER_TUR | GMAC_IER_RLEX | GMAC_IER_TFC)
@@ -414,8 +409,12 @@ void gmacd_configure(struct _ethd * gmacd,
 
 	/* Enable the copy of data into the buffers
 	   ignore broadcasts, and don't copy FCS. */
-	ncfgr = gmac_get_network_config_register(gmac);
-	ncfgr |= GMAC_NCFGR_FD | GMAC_NCFGR_DBW_DBW64;
+	ncfgr = gmac_get_network_config_register(gmac) & (~GMAC_NCFGR_DBW_Msk);
+	#if defined(CONFIG_SOC_SAMA5D3)
+		ncfgr |= GMAC_NCFGR_FD | GMAC_NCFGR_DBW_DBW64;
+	#else
+		ncfgr |= GMAC_NCFGR_FD | GMAC_NCFGR_DBW_DBW32;
+	#endif
 	if (enableCAF) {
 		ncfgr |= GMAC_NCFGR_CAF;
 	}
