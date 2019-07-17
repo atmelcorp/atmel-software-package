284ca463620901bd122f6cc24d092f6a0045c46d
Jeff Lin <min.lin@microchip.com>
2019-07-17 16:33:31 +0800

    target/sama5d3: add restrictions code for ddram
    Signed-off-by: Jeff Lin <min.lin@microchip.com>

--- target/sama5d3/board_support.c
+++ target/sama5d3/board_support.c
@@ -230,7 +230,9 @@ void board_save_misc_power(void)
 #endif
 
 	/* disable system clocks */
+#ifdef VARIANT_SRAM
 	pmc_disable_system_clock(PMC_SYSTEM_CLOCK_DDR);
+#endif
 	pmc_disable_system_clock(PMC_SYSTEM_CLOCK_LCD);
 	pmc_disable_system_clock(PMC_SYSTEM_CLOCK_SMD);
 	pmc_disable_system_clock(PMC_SYSTEM_CLOCK_UHP);
@@ -248,7 +250,10 @@ void board_save_misc_power(void)
 			continue;
 		if (i == tc_id)
 			continue;
-
+#ifdef VARIANT_DDRAM
+		if (i == ID_MPDDRC)
+			continue;
+#endif
 		pmc_disable_peripheral(i);
 	}
 }
