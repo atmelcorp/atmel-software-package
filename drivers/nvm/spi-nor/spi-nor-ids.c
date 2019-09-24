d3bff280a16a32547fe8a65f477c916430e2843d
Cyrille Pitchen <cyrille.pitchen@microchip.com>
2019-09-24 16:03:40 +0200

    drivers/nvm/spi-nor: support Macronix MX66L2G45G
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@microchip.com>

--- drivers/nvm/spi-nor/spi-nor-ids.c
+++ drivers/nvm/spi-nor/spi-nor-ids.c
@@ -205,6 +205,7 @@ const struct spi_nor_info spi_nor_ids[] = {
 	{ MX25L("mx25l25673f", 0xc22019,  512), },
 	{ MX25L("mx25l51245f", 0xc2201a, 1024), },
 	{ MX66L("mx66l1g45g",  0xc2201b, 2048), },
+	{ MX66L("mx66l2g45g",  0xc2201c, 4096), },
 	{ MX25("mx25u1635f",   0xc23525,   32), },
 
 	/* SST */
